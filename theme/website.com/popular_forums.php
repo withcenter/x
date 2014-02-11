50baa2da482239bbc453bae9617484bb2b8d514d
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-02-11 17:44:23 +0800

    popular posts and new websites

--- theme/website.com/popular_forums.php
+++ theme/website.com/popular_forums.php
@@ -25,7 +25,7 @@
 		$post_url = g::url()."/bbs/board.php?bo_table=".$allhits[$count]['bo_table']."&wr_id=".$allhits[$count]['wr_id'];
 		?>
 			<div class='info-wrapper'>
-				<div class='top-info bo-table'><a href='<?=$post_url?>'><?=$allhits[$count]['bo_table']?></a></div>
+				<div class='top-info bo-table'><a href='<?=$post_url?>'><?=$count+1?>) <?=$allhits[$count]['bo_table']?></a></div>
 				<div class='other-info subject'><a href='<?=$post_url?>'><?=substr($allhits[$count]['wr_subject'],0,10)?> (Views: <?=$allhits[$count]['wr_hit']?> )</a></div>
 				<div class='other-info content'><a href='<?=$post_url?>'><?=$allhits[$count]['wr_content']?></a></div>
 			</div>
