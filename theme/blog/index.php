4d40d850c5701511ae4b8456ccaac23fdc6e7ae8
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-02-12 21:56:14 +0800

    initial transition to ms::meta

--- theme/blog/index.php
+++ theme/blog/index.php
@@ -1,10 +1,9 @@
 <?php
-$extra = ms::get_extra();
 	/** display 5 posts from each forum that is selected by the user on Config_Global */
 	for ( $i = 1; $i <= 10; $i++ ) { 
-		if ( $extra['forum_no_'.$i] != '' ) {
-			$option = db::row( "SELECT bo_subject, bo_count_write FROM $g5[board_table] WHERE bo_table='".$extra['forum_no_'.$i]."'" );
-			echo latest( "x-latest-blog" , $extra['forum_no_'.$i] , 5 , 25 );
+		if ( ms::meta('forum_no_'.$i) != '' ) {
+			$option = db::row( "SELECT bo_subject, bo_count_write FROM $g5[board_table] WHERE bo_table='".ms::meta('forum_no_'.$i)."'" );
+			echo latest( "x-latest-blog" , ms::meta('forum_no_'.$i) , 5 , 25 );
 		}
 	}
 
