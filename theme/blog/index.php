dffad173a1d6e9165af5f11684b6900a20db9c38
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-02-12 18:03:43 +0800

    blog theme, fetch posts from config global forum dropdown menus, please clone x-latest-blog too

--- theme/blog/index.php
+++ theme/blog/index.php
@@ -1,2 +1,10 @@
 <? 
-	echo latest("x-latest-blog",$extra['menu_1'], 3, 25);
+	/** display 5 posts from each forum that is selected by the user on Config_Global */
+	for ( $i = 1; $i <= 10; $i++ ) { 
+		if ( $extra['forum_no_'.$i] != '' ) {
+			$option = db::row( "SELECT bo_subject, bo_count_write FROM $g5[board_table] WHERE bo_table='".$extra['forum_no_'.$i]."'" );
+			echo latest( "x-latest-blog" , $extra['forum_no_'.$i] , 5 , 25 );
+		}
+	}
+
+
