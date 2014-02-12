36d98e413f353670fefa7e55481770afeefe46a0
yonggyo00 <lyonggyo@gmail.com>
2014-02-12 15:41:14 +0800

    x

--- module/multisite/my_sites.php
+++ module/multisite/my_sites.php
@@ -4,20 +4,20 @@
 
 <div class='view-site'>
 	<div>
-		<h2> Site List </h2>
+		<div class='title'><div class='inner'>사이트 리스트</div></div>
 		<ul>
 			<?php
 				$i = 0;
 				foreach ( ms::my_site() as $site ) {
 				$i++;				
-				$date_created = date("(T) m/d/Y, h:iA",($site['stamp_create']));
+				$date_created = date("Y-m-d H:i:s A",($site['stamp_create']));
 				if ( count( ms::my_site() ) == $i ) $is_last_count = "last-count";
 			 ?>
 				<li class='<?=$is_last_count?>'>
 					<a href="<?=ms::url_site($site['domain'])?>">
-						<div class='info'>Domain Name: <?=$site['domain']?></div>
-						<div class='info'>Site Title: <?=$site['title']?></div>
-						<div class='info'>Date Created: <?=$date_created?></div>				
+						<div class='info'><span>사이트 주소</span><?=$site['domain']?></div>
+						<div class='info'><span>사이트 제목</span><?=$site['title']?></div>
+						<div class='info'><span>생성일</span><?=$date_created?></div>				
 					</a>
 				</li>
 			<? }?>
--- theme/website.com/tail.php
+++ theme/website.com/tail.php
@@ -1,10 +1,11 @@
 </div>
 <div style='clear:both;'></div>
 	<div id='footer'>
+	<? /*
 		<div id='bottom-menus'>
-		<a class='home' href='/' class="menu-list"><?=ln('HOME')?></a></li>
+		<a class='home' href='/' class="menu-list">홈</a></li>
 		<?php if ( $is_admin == 'super' || ms::admin()) { ?>
-			<a href="<?=x::url()?>/?module=admin&action=index" class="admin"><?=ln('ADMIN PAGE')?></a>		
+			<a href="<?=x::url()?>/?module=admin&action=index" class="admin">어드민 패널</a>		
 		<? } ?>
 		<? for ( $i = 1; $i <= 10; $i++ ) { ?>	
 		<? if ( $extra['menu_'.$i] != '' ) {
@@ -14,10 +15,14 @@
 			else $menu_id = null;		
 		?>
 			<a class='<?php echo $extra['menu_'.$i]?>' href='<?=g::url()?>/bbs/board.php?bo_table=<?=$extra['menu_'.$i]?>' class="menu-list"><?=strtoupper($option['bo_subject'])?></a>			
-		<?}}?>
-		<a href="<?=ms::url_main_site()?>" class="main-site"><?=ln('MAIN SITE')?></a>
+		<?}} ?>
+		
+		<a href="http://wwww.philgo.com" class="main-site">필고</a>
 		</div>
-		<div id='copyright'>COPYRIGHT Ⓒ 2014 ALL RIGHTS RESRERVED WEBSITE.COM</div>
+		*/?>
+			<b>상호</b> 위세너 <b>대표자</b> 송재호 <b>대표전화</b>070-7529-1749 <b>이메일</b>philgohelp@gmail.com<br />
+			<b>사업자등록번호</b> 106-02-98669 <b>통신판매신고번호</b> 2008-경남김해-0098<br />
+			<b>주소</b> 경상남도 김해시 한림면 신천리 284 <b>개인정보관리책임자</b> 송재호<br />
 	</div>
 </div>
 </div>
