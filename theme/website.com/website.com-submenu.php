7e8f51b4c31089a3a2942c43210a15be69ecf63d
yonggyo00 <lyonggyo@gmail.com>
2014-02-12 15:49:57 +0800

    x

--- theme/website.com/website.com-submenu.php
+++ theme/website.com/website.com-submenu.php
@@ -1,3 +1,3 @@
 <a href='<?=g::url()?>'>홈</a>
 <a href='<?=g::url()?>/bbs/board.php?bo_table=qna'>질문과답변</a>
-<a href='<?=g::url()?>/bbs/board.php?bo_table=support'>이용안내</a>
+<a href='<?=g::url()?>/bbs/board.php?bo_table=help'>이용안내</a>
