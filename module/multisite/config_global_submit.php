70fa3b14592128cfb511ec392e69d837992f5dc3
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-02-12 17:09:59 +0800

    config_global_submit, fixed mkdir for $root and $path variables

--- module/multisite/config_global_submit.php
+++ module/multisite/config_global_submit.php
@@ -1,13 +1,19 @@
 <?php
-/* upload Global Settings Files ex. header_logo, banner_1, banner_2 to g5/data/upload/multisite/site_name,
-   note: I haven't tried making a create thumbnail or image resize function yet.
-*/
+/* upload Global Settings Files ex. header_logo, banner_1, banner_2 to g5/data/upload/multisite/site_name/file_name.ext */
 
-$path = G5_PATH.'/'.G5_DATA_DIR.'/upload/multisite/';
+$root = G5_PATH.'/'.G5_DATA_DIR.'/upload/';
+$path = $root.'multisite/';
 $site = explode( '.' , etc::domain());
 $folder = $path.$site[0].'/';
+
+if(!is_dir($path)) { 
+	mkdir($root);
+	mkdir($path);
+}
 if(!is_dir($folder)) mkdir($folder);
+
 $in['img_url'] = G5_DATA_DIR.'/upload/multisite/'.$site[0].'/';
+
 foreach($_FILES as $key => $value) { 
 	$file_info = pathinfo($value['name']);
 	if( $name = $file_info['filename'] ) {
@@ -17,5 +23,6 @@ foreach($_FILES as $key => $value) {
 		move_uploaded_file( $_FILES[$key]['tmp_name'], $target);
 	}
 }
+
 ms::update( $in );
 jsGo("?module=$module&action=config_global");
