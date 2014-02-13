bb4c92eab6d737c79118a98104fa87929a0dbaa5
lyonggyo@gmail.com <lyonggyo@gmail.com>
2014-02-13 16:06:56 +0800

    x

--- module/multisite/create_submit.php
+++ module/multisite/create_submit.php
@@ -2,7 +2,6 @@
 	
 	$domain = $sub_domain . '.' . etc::base_domain();
 	
-	
 	if ( $error_code = ms::create( array('domain'=>$domain, 'title'=>$title) ) ) include module( 'create_fail' );
 	else {
 		$o = array(
@@ -11,5 +10,18 @@
 			'group_id'	=> 'multisite',
 		);
 		g::board_create($o);
+		
+		
+		$meta_op = array(
+						'domain' => $domain,
+						'code'=>'theme',
+						'value'=>'blog'
+		);
+		db::insert('x_multisite_meta', $meta_op );
+
+		$theme = 'blog';
+		$priority = 10;
+		md::config_update();
+		
 		include module( 'create_success' );
 	}
