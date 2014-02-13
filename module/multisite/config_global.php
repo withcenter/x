f7f3e4dd4b7f8d9276b356538451c3d645d476eb
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-02-13 18:34:20 +0800

    quick fixes

--- module/multisite/config_global.php
+++ module/multisite/config_global.php
@@ -112,12 +112,13 @@
 						<div class='title'>로고 문구</div>
 						<textarea name='logo_text'><?=stripslashes(ms::meta('logo_text'))?></textarea>
 					</td>
+				</tr>
 				<tr valign='top'>
 					<td>
 						<div class='title'>배너이미지1</div>
 						<?if( ms::meta('banner_1') ) {?><img src="<?=ms::url_site(etc::domain()).'/'.ms::meta('img_url').ms::meta('banner_1')?>" width=280px height=160px><br><?}?>
 						<input type='file' name='banner_1'>
-						<?if( ms::meta('header_logo') != '' ) { ?>
+						<?if( ms::meta('banner_1') != '' ) { ?>
 							<input type='hidden' name='banner_1_remove' value='n'>
 							<input type='checkbox' name='banner_1_remove' value='y'>Remove Image
 						<?}?>
@@ -129,9 +130,9 @@
 
 					<td>
 						<div class='title'>배너이미지2</div>
-						<?if( ms::meta('banner_1') ) {?><img src="<?=ms::url_site(etc::domain()).'/'.ms::meta('img_url').ms::meta('banner_2')?>" width=280px height=160px><br><?}?>
+						<?if( ms::meta('banner_2') ) {?><img src="<?=ms::url_site(etc::domain()).'/'.ms::meta('img_url').ms::meta('banner_2')?>" width=280px height=160px><br><?}?>
 						<input type='file' name='banner_2'>
-						<?if( ms::meta('header_logo') != '' ) { ?>
+						<?if( ms::meta('banner_2') != '' ) { ?>
 							<input type='hidden' name='banner_2_remove' value='n'>
 							<input type='checkbox' name='banner_2_remove' value='y'>Remove Image
 						<?}?>
