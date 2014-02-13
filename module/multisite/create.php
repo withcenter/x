8a9fc04e22b81482d24959adf321cafec6945bf8
lyonggyo@gmail.com <lyonggyo@gmail.com>
2014-02-13 17:44:52 +0800

    x

--- module/multisite/create.php
+++ module/multisite/create.php
@@ -11,6 +11,7 @@
 					<div><span class='item'>사이트 주소</span> http://<input type='text' name='sub_domain'>.<?=etc::base_domain()?></div>
 					<div><span class='item'>사이트 제목</span> <input type='text' name='title'></div>
 					<input type='submit' value='생성'>
+					<div style='clear:right;'></div>
 			</form>
 	</div>
 	<div>
