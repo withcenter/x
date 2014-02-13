7570df2457babb63b32e4883073ddd2083d23297
lyonggyo@gmail.com <lyonggyo@gmail.com>
2014-02-13 19:23:01 +0800

    x

--- theme/website.com/index.php
+++ theme/website.com/index.php
@@ -1,31 +1,39 @@
 <div id='howto-steps'>
 	<table cellspacing=0 cellpadding=0 width='100%'><tr valign='top'>
 		<td>
-			<div class='steps first'>
-				<img class='step1-img' src='<?=x::url()?>/theme/website.com/img/step1.png'/>
-				<div class='description'>
-					<div class='title'>필고 홈페이지</div>
-					<div class='instruction'>(모바일) 홈페이지, 스마트폰 앱,<br />바이럴 마케팅 프로그램<br />무료 제공</div>
+			<a href='<?=g::url()?>/bbs/board.php?bo_table=help&wr_id=1'>
+				<div class='steps first'>
+					<img class='step1-img' src='<?=x::url()?>/theme/website.com/img/step1.png'/>
+					<div class='description'>
+						<div class='title'>필고 홈페이지</div>
+						<div class='instruction'>(모바일) 홈페이지, 스마트폰 앱,<br />바이럴 마케팅 프로그램<br />무료 제공</div>
+					</div>
 				</div>
-			</div>
+			</a>
 		</td>
 		<td>
 			<div class='steps second'>
 				<img class='step2-img' src='<?=x::url()?>/theme/website.com/img/step2.png'/>
 				<div class='description'>
 					<div class='title'>사이트 만들기 예제</div>
-					<div class='instruction'>여행사 만들기<br />커뮤니티 사이트 만들기<br />블로그 만들기</div>
+					<div class='instruction'>
+						<a href='<?=g::url()?>/bbs/board.php?bo_table=help&wr_id=2'>여행사 만들기</a><br />
+						<a href='<?=g::url()?>/bbs/board.php?bo_table=help&wr_id=3'>커뮤니티 사이트 만들기</a><br />
+						<a href='<?=g::url()?>/bbs/board.php?bo_table=help&wr_id=4'>블로그 만들기</a></div>
 				</div>
 			</div>
 		</td>
 		<td>
-			<div class='steps third'>
-				<img class='step3-img' src='<?=x::url()?>/theme/website.com/img/step3.png'/>
-				<div class='description'>
-					<div class='title'>도와주세요</div>
-					<div class='instruction'>사이트 만들기가 어려우면<br />여기를 클릭!</div>
+			<a href='<?=g::url()?>/bbs/board.php?bo_table=help&wr_id=5'>
+				<div class='steps third'>
+					<img class='step3-img' src='<?=x::url()?>/theme/website.com/img/step3.png'/>
+					<div class='description'>
+						<div class='title'>도와주세요</div>
+						<div class='instruction'>사이트 만들기가 어려우면<br />여기를 클릭!</div>
+			
+					</div>
 				</div>
-			</div>
+			</a>
 		</td>
 	</tr></table>
 </div>
