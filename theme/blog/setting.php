aa077529adc80afce7152be9bc7f6f73dbe51b55
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-02-13 17:22:40 +0800

    adjusted config_global && config_global_submit file upload function to work properly with ms::meta, also added blog extra option of profile picture

--- theme/blog/setting.php
+++ theme/blog/setting.php
@@ -1,6 +1,14 @@
-Extra Options
-<div class='title'>프로필 이미지</div>
-<?if( ms::meta('blog_profile_photo') ) {?><img src="<?=ms::url_site(etc::domain()).'/'.ms::meta('img_url').ms::meta('blog_profile_photo')?>" width=280px height=160px><br><?}?>
-<input type='file' name='blog_profile_photo'><br>
-Profile Message: <br>
-<input type='text' name='blog_profile_message' value='<?=ms::meta('blog_profile_message')?>'>
+<div class='title'>Extra Options</div>
+<table cellpadding=5px>
+	<tr>
+		<td valign='top'>
+			<h2>Blog Profile Photo</h2>
+			<?if( ms::meta('blog_profile_photo') ) {?><img src="<?=ms::url_site(etc::domain()).'/'.ms::meta('img_url').ms::meta('blog_profile_photo')?>" width=280px height=160px><br><br><?}?>
+			<input type='file' name='blog_profile_photo'>
+		</td>
+		<td  valign='top'>
+			<h2>Blog Profile Message</h2>
+			<textarea name='blog_profile_message'><?=stripslashes(ms::meta('blog_profile_message'))?></textarea>
+		</td>
+	</tr>
+</table>
