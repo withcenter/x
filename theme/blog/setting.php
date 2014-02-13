ceba08f7169d493f35720ea02bf5eb7ba94284ec
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-02-13 18:14:50 +0800

    added remove image checkbox per image, i only set ms::meta('file-upload-name','') to empty, i did not made a function to remove the file from  the folder

--- theme/blog/setting.php
+++ theme/blog/setting.php
@@ -5,6 +5,10 @@
 			<h2>Blog Profile Photo</h2>
 			<?if( ms::meta('blog_profile_photo') ) {?><img src="<?=ms::url_site(etc::domain()).'/'.ms::meta('img_url').ms::meta('blog_profile_photo')?>" width=280px height=160px><br><br><?}?>
 			<input type='file' name='blog_profile_photo'>
+			<?if( ms::meta('blog_profile_photo') != '' ) { ?>
+				<input type='hidden' name='blog_profile_photo_remove' value='n'>
+				<input type='checkbox' name='blog_profile_photo_remove' value='y'>Remove Image
+			<?}?>
 		</td>
 		<td  valign='top'>
 			<h2>Blog Profile Message</h2>
