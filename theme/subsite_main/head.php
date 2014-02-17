c77aba14afda9373e860e1239b04ed16310ec8e7
yongggyo00 <lyonggyo@gmail.com>
2014-02-17 21:58:28 +0800

    adding theme of subsite_main

---
+++ theme/subsite_main/head.php
@@ -0,0 +1,80 @@
+<link rel="stylesheet" href="<?=x::url_theme()?>/css/theme.css">
+<!-- 상단 시작 { -->
+<div id='website-wrapper'>
+<div id="hd">
+    <h1 id="hd_h1"><?php echo $g5['title'] ?></h1>
+    <div id="hd-wrapper">
+		<div id='menu-top'>
+			<div id="logo">
+				<a href="<?php echo G5_URL ?>">
+					<img src='<?=x::url_theme()?>/img/logo.png'>
+				</a>
+			</div>
+			<?include x::theme('website.com-menu')?>
+		</div>
+		<div id='search-wrapper'>
+			<div class='sub-menu'>
+				<?include x::theme('website.com-submenu')?>
+			</div>
+			<div id='search-box'>
+				<fieldset id="search-fieldset">
+					<form name="fsearchbox" method="get" action="<?php echo G5_BBS_URL ?>/search.php" onsubmit="return fsearchbox_submit(this);">
+					<input type="hidden" name="sfl" value="wr_subject||wr_content">
+					<input type="hidden" name="sop" value="and">
+					<input type="text" name="stx" id="search_txtbox" maxlength="20" placeholder='Search'>
+					<input type="image" id="search_submit" src="/x/theme/website.com/img/search.png">
+					</form>
+
+					<script>
+					function fsearchbox_submit(f)
+					{
+						if (f.stx.value.length < 2) {
+							alert("검색어는 두글자 이상 입력하십시오.");
+							f.stx.select();
+							f.stx.focus();
+							return false;
+						}
+
+						// 검색에 많은 부하가 걸리는 경우 이 주석을 제거하세요.
+						var cnt = 0;
+						for (var i=0; i<f.stx.value.length; i++) {
+							if (f.stx.value.charAt(i) == ' ')
+								cnt++;
+						}
+
+						if (cnt > 1) {
+							alert("빠른 검색을 위하여 검색어에 공백은 한개만 입력할 수 있습니다.");
+							f.stx.select();
+							f.stx.focus();
+							return false;
+						}
+
+						return true;
+					}
+					</script>
+				</fieldset>	
+			</div>
+		</div>
+    </div>
+</div>
+<!-- } 상단 끝 -->
+
+<hr>
+
+<!-- 콘텐츠 시작 { -->
+
+<div id="wrapper">
+	<table cellpadding=0 cellspacing=0 width='100%'>
+		<tr valign='top'>
+			<td>
+				<div id="aside">
+					<?php echo outlogin('x-outlogin-website.com'); // 외부 로그인  ?>
+					<?php echo poll('basic'); // 설문조사  ?>
+					<?php include "popular_forums.php"; ?>
+					<?php include "new_sites.php"; ?>
+				</div>
+			</td>
+			<td>
+				<div id="container">
+					<?if ( (preg_match('/^config/', $action)) || (preg_match('/^config_/', $action)) ) include ms::site_menu();?>
+					<?php if ((!$bo_table || $w == 's' ) && !defined("_INDEX_")) { ?><div id="container_title"><?php echo $g5['title'] ?></div><?php } ?>
---
+++ theme/subsite_main/website.com-menu.php
@@ -0,0 +1,64 @@
+<link rel='stylesheet' type='text/css' href='<?=x::url_theme()?>/css/menu.css' />
+
+<?php
+
+	$main = array();
+
+?>	
+<style>
+	#website-com-menu  {
+		font-family: '맑은 고딕', AppleGothic;
+	}
+</style>
+<ul id="website-com-menu">		
+	<?php
+		$i = 0;
+		foreach ( $main as $row ) {
+			
+	 ?>
+	<?php } 
+		if ( $is_admin == 'super' ){
+	?>
+	<li class="menu-list">
+		<a href="<?=x::url()?>/?module=admin&action=index" class="admin">어드민패널</a>
+	</li>
+	<?}?>
+	
+	<? if ( ms::admin() ) {?>
+		<li class='menu-list'><a class='my-site-setting' href='<?=ms::url_config()?>'>사이트 설정</a></li>
+	<? }?>
+	
+	<li class="menu-list"><a class='my-sites' href='<?=x::url()?>/?module=multisite&action=my_sites'>내 사이트</a></li>
+	
+	<? /*
+	<? for ( $i = 1; $i <= 10; $i++ ) { ?>
+	
+	<? if ( $extra['menu_'.$i] != '' ) {
+		$option = db::row("SELECT bo_subject FROM $g5[board_table] WHERE bo_table = '".$extra['menu_'.$i]."'");
+	
+		if ( $extra['submenu_'.$i] ) $menu_id = "menu=$i";
+		else $menu_id = null;		
+	?>
+		<li class="menu-list" <?=$menu_id?>><a class='<?php echo $extra['menu_'.$i]?>' href='<?=g::url()?>/bbs/board.php?bo_table=<?=$extra['menu_'.$i]?>' class="menu-list" <?// if ($extra['menu'.$i.'_target'] == 'Y') echo "target='_blank'"?>><?=$option['bo_subject']?></a></li>				
+	<?}}?>
+	*/?>
+	
+	<li class="menu-list">
+		<a href="http://philgo.com" target='_blank' class="main-site">필고</a>
+	</li>
+</ul>
+<script>
+
+	$(function(){			
+		if( ("<?php echo $in['bo_table'];?>")){
+			$(".menu-list .<?php echo $in['bo_table'];?>").addClass('selected');
+		}
+		else if ( "<?php echo $in['action']?>" == "my_sites" ){
+			$(".menu-list .my-sites").addClass('selected');
+		}
+		else if ( "<?php echo $in['module']?>" == "admin" || "<?php echo $in['module']?>" == "multisite" || "<?php echo $in['module']?>" == "multidomain" ){
+			$(".menu-list .admin").addClass('selected');
+		}		
+	});
+
+</script>
