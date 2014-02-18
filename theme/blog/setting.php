308e2e708440990074ae5ad14b12c42bc1a898cd
yonggyo00 <lyonggyo@gmail.com>
2014-02-18 19:41:17 +0800

    blog setting.php

--- theme/blog/setting.php
+++ theme/blog/setting.php
@@ -1,7 +1,7 @@
 <table cellpadding=5px class='image-config'>
 	<tr>
 		<td valign='top'>
-			<div class='title'>Blog Profile Photo</div>
+			<div class='title'>프로필 사진등록</div>
 			<?if( ms::meta('blog_profile_photo') ) {?><img src="<?=ms::meta('img_url').ms::meta('blog_profile_photo')?>" width=280px height=160px><br><br><?}?>
 			<input type='file' name='blog_profile_photo'>
 			<?if( ms::meta('blog_profile_photo') != '' ) { ?>
@@ -10,7 +10,7 @@
 			<?}?>
 		</td>
 		<td  valign='top'>
-			<div class='title'>Blog Profile Message</div>
+			<div class='title'>프로필 하단 문구</div>
 			<textarea name='blog_profile_message'><?=stripslashes(ms::meta('blog_profile_message'))?></textarea>
 		</td>
 	</tr>
