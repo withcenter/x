1022c27ef26fe03bad9f1d2dc7aca617d5734ea9
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-02-18 15:30:37 +0800

    dasda

--- theme/travel_study1/head.php
+++ theme/travel_study1/head.php
@@ -6,57 +6,67 @@
 
     <div id="skip_to_container"><a href="#container">본문 바로가기</a></div>
 
-    <div id="hd_wrapper">
+    <div id="travel-hd-wrapper">
+		<div id="top-menu">
+			<div class='inner'>
+				<a href='#'>LOG IN</a>
+				<a href='#'>SIGN UP</a>
+				<a href='#'>CUSTOMER SERVICE</a>
+				<div class='customer-support'><img src='<?=x::url_theme()?>/img/phone.png'/>CUSTOMER SUPPORT: 123-45-67</div>
+			</div>
+		</div>
+		
+		<div id='logo-and-search'>
+			<div class='inner'>
+				<div id="logo">
+					<a href="<?php echo G5_URL ?>">
+					<?if( ms::meta('header_logo') ) { ?>
+						<img src="<?=ms::meta('img_url').ms::meta('header_logo')?>">
+					<?} else {?>
+						<img src='<?=x::url_theme()?>/img/travel_sample_banner.png'>
+					<?}?>
+					</a>
+				</div>
+					<fieldset id="hd_sch">
+						<legend>사이트 내 전체검색</legend>
+						<form name="fsearchbox" method="get" action="<?php echo G5_BBS_URL ?>/search.php" onsubmit="return fsearchbox_submit(this);">
+						<input type="hidden" name="sfl" value="wr_subject||wr_content">
+						<input type="hidden" name="sop" value="and">
+						<label for="sch_stx" class="sound_only">검색어<strong class="sound_only"> 필수</strong></label>
+						<input type="text" name="stx" id="sch_stx" maxlength="20">
+						<input type="submit" id="sch_submit" value="Go!">
+						</form>
 
-        <div id="logo">
-            <a href="<?php echo G5_URL ?>">
-			<?if( ms::meta('header_logo') ) { ?>
-				<img src="<?=ms::meta('img_url').ms::meta('header_logo')?>">
-			<?} else {?>
-				<img src='<?=x::url_theme()?>/img/travel_sample_banner.png'>
-			<?}?>
-			</a>
-        </div>
+						<script>
+						function fsearchbox_submit(f)
+						{
+							if (f.stx.value.length < 2) {
+								alert("검색어는 두글자 이상 입력하십시오.");
+								f.stx.select();
+								f.stx.focus();
+								return false;
+							}
 
-        <fieldset id="hd_sch">
-            <legend>사이트 내 전체검색</legend>
-            <form name="fsearchbox" method="get" action="<?php echo G5_BBS_URL ?>/search.php" onsubmit="return fsearchbox_submit(this);">
-            <input type="hidden" name="sfl" value="wr_subject||wr_content">
-            <input type="hidden" name="sop" value="and">
-            <label for="sch_stx" class="sound_only">검색어<strong class="sound_only"> 필수</strong></label>
-            <input type="text" name="stx" id="sch_stx" maxlength="20">
-            <input type="submit" id="sch_submit" value="검색">
-            </form>
+							// 검색에 많은 부하가 걸리는 경우 이 주석을 제거하세요.
+							var cnt = 0;
+							for (var i=0; i<f.stx.value.length; i++) {
+								if (f.stx.value.charAt(i) == ' ')
+									cnt++;
+							}
 
-            <script>
-            function fsearchbox_submit(f)
-            {
-                if (f.stx.value.length < 2) {
-                    alert("검색어는 두글자 이상 입력하십시오.");
-                    f.stx.select();
-                    f.stx.focus();
-                    return false;
-                }
+							if (cnt > 1) {
+								alert("빠른 검색을 위하여 검색어에 공백은 한개만 입력할 수 있습니다.");
+								f.stx.select();
+								f.stx.focus();
+								return false;
+							}
 
-                // 검색에 많은 부하가 걸리는 경우 이 주석을 제거하세요.
-                var cnt = 0;
-                for (var i=0; i<f.stx.value.length; i++) {
-                    if (f.stx.value.charAt(i) == ' ')
-                        cnt++;
-                }
-
-                if (cnt > 1) {
-                    alert("빠른 검색을 위하여 검색어에 공백은 한개만 입력할 수 있습니다.");
-                    f.stx.select();
-                    f.stx.focus();
-                    return false;
-                }
-
-                return true;
-            }
-            </script>
-        </fieldset>
-
+							return true;
+						}
+						</script>
+					</fieldset>
+			</div>
+		</div>
 
 
 		
