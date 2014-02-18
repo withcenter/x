b5ffe4350aa0483fd5e41868f8862a149159baa5
arvindomingo <arvin.o.domingo@gmail.com>
2014-02-18 08:34:41 +0000

    x

--- theme/travel_theme_1/index.php
+++ theme/travel_theme_1/index.php
@@ -14,14 +14,28 @@
 	</div>
 	
 	<div class='forum-list'>
+		<div style='padding: 1em'>
 		<div class='discussion-forum'>
-			<h2> Discussion Forum </h2>
+			<h2><img src='<?=x::url_theme()?>/img/discussion-icon.png'> Discussion Forum </h2>
+			<ul>
+				<li><a href='#'>POST TITLE 1 HERE<br><p>Brief Detail about the post here.</p></a></li>
+				<li><a href='#'>POST TITLE 2 HERE<br><p>Brief Detail about the post here.</p></a></li>
+			</ul>
 		<div>
 		<div class='qna'>
-			<h2> Q&As </h2>
+			<h2><img src='<?=x::url_theme()?>/img/qna.png'> Q&As </h2>
+			<ul>
+				<li><a href='#'>QUESTION 1 HERE<br><p>Brief Detail about the answer here.</p></a></li>
+				<li><a href='#'>QIESTION 2 HERE<br><p>Brief Detail about the answer here.</p></a></li>
+			</ul>
 		</div>
 		<div class='travel-forum'>
-			<h2> Travel Stories </h2>
+			<h2><img src='<?=x::url_theme()?>/img/travel.png'> Travel Stories </h2>
+			<ul>
+				<li><a href='#'>TRAVEL STORY 1 HERE<br><p>Brief Detail about the post here.</p></a></li>
+				<li><a href='#'>TRAVEL STORY 2 HERE<br><p>Brief Detail about the post here.</p></a></li>
+			</ul>
+		</div>
 		</div>
 	</div>
 </div>
