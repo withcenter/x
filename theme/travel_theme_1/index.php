40502be49fcd7f7e6ee016b2282f4602395b1c0d
arvindomingo <arvin.o.domingo@gmail.com>
2014-02-18 08:02:42 +0000

    Travel Index Page

--- theme/travel_theme_1/index.php
+++ theme/travel_theme_1/index.php
@@ -1,1 +1,27 @@
-<h1 style="font-size: 10em;"> This is index.php </h1>
+<div class='top-panel'>
+	<div class='banner'>
+		<?
+			for ( $i = 1 ; $i <= 5 ; $i++ ) {
+				echo "<div class='banner-image'>";
+				echo "<img src='".ms::meta('img_url').ms::meta('travelbanner_'.$i)."'>";
+				echo "<p class='banner-text'>".ms::meta('travelbanner_'.$i.'_text1')."</p>";
+				echo "</div>";
+			}
+		?>
+		<div class='banner-pagination'>
+			pagination here
+		</div>
+	</div>
+	
+	<div class='forum-list'>
+		<div class='discussion-forum'>
+			<h2> Discussion Forum </h2>
+		<div>
+		<div class='qna'>
+			<h2> Q&As </h2>
+		</div>
+		<div class='travel-forum'>
+			<h2> Travel Stories </h2>
+		</div>
+	</div>
+</div>
