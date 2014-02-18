a2f7a82038eecae63f1836e4fc692a19e0cae8e0
yonggyo00 <lyonggyo@gmail.com>
2014-02-18 15:25:32 +0800

    work for user_site_list.php

--- theme/subsite_main/user_site_list.php
+++ theme/subsite_main/user_site_list.php
@@ -1,18 +1,20 @@
 <? if ( $member['mb_id'] ) {
 	$my_site = ms::my_site();
+	if ( $my_site ) {
 ?>
 <link rel='stylesheet' type='text/css' href='<?=x::url_theme()?>/css/user_site_list.css' />
-<div class='user-site-list'>
-	<div class='title'><a href='<?=x::url()?>/?module=multisite&action=my_sites'>내 사이트 바로가기</a></div>
-	<?php
-	foreach ( $my_site as $s ) {
-		$site_url = ms::url_site($s['domain']);
-		echo "
-			<div class='row'>
-				<a href='$site_url'>$s[title]</a>
-			</div>
-		";
+	<div class='user-site-list'>
+		<div class='title'><a href='<?=x::url()?>/?module=multisite&action=my_sites'>내 사이트 바로가기</a></div>
+		<?php
+		foreach ( $my_site as $s ) {
+			$site_url = ms::url_site($s['domain']);
+			echo "
+				<div class='row'>
+					<a href='$site_url'>$s[title]</a>
+				</div>
+			";
+		}
+	echo "</div>";
 	}
 }
 ?>
-</div>
