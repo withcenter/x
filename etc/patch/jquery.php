90b84f5e2f01572e2d03b09734722c1f6f1834d9
JaeHo Song <thruthesky@gmail.com>
2014-02-20 19:26:59 +0900

    patching to use board skin of x

--- etc/patch/jquery.php
+++ etc/patch/jquery.php
@@ -17,10 +17,10 @@
 	
 	$dst =<<<EOP
 	<!--[if lt IE 9]>
-		<script type='text/javascript' src='<?php echo G5_URL ?>/x/js/jquery-1.11.0-rc1.js'></script>
+		<script type='text/javascript' src='<?php echo G5_URL ?>/x/js/jquery-1.11.0.min.js'></script>
 	<![endif]-->
 	<!--[if gte IE 9]><!-->
-		<script type='text/javascript' src='<?php echo G5_URL ?>/x/js/jquery-2.1.0-rc1.js'></script>
+		<script type='text/javascript' src='<?php echo G5_URL ?>/x/js/jquery-2.1.0.min.js'></script>
 	<!--<![endif]-->
 EOP;
 
