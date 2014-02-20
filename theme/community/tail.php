4e889808c5eb87a231b8e676789ff0dc3c66ab80
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-02-20 19:27:50 +0800

    community theme

--- theme/community/tail.php
+++ theme/community/tail.php
@@ -1,18 +1,20 @@
 
-    </div>
+</div>
+<div class='footer'>
+	<div class='footer-logo'>
+		<?if( ms::meta('comfooter_logo') ) $company_banner = ms::meta('img_url').ms::meta('comfooter_logo');
+		else $company_banner = x::url_theme().'/img/community_footer_logo.png';?>
+		<img src="<?=$company_banner?>">		
+	</div>
+	<div class='footer-text'>
+	<? if( ms::meta('footer_text') == '' ) {?>
+			<div class='footer-links'><a href='#'>ABOUT</a><a href='#'>TERMS & CONDITIONS</a><a href='#'>PRIVACY POLICY</a><a href='#'>FAQS</a></div>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>COPYRIGHT @ 2014 &nbsp&nbsp ALL RIGHTS RESERVED &nbsp&nbsp WITHCENTER.COM
+	<?} else echo ms::meta('footer_text');?>
+	</div>
 </div>
 
-<!-- } 콘텐츠 끝 -->
-
-<hr>
-
-<!-- 하단 시작 { -->
-<!--<div id="ft">
-
-</div>-->
-
-
-<!-- } 하단 끝 -->
+<!--  하단 끝 -->
 
 <script>
 $(function() {
@@ -21,6 +23,4 @@ $(function() {
 });
 </script>
 
-<?php
-include_once(G5_PATH."/tail.sub.php");
-?>
+<?debug::mode(0)?>
