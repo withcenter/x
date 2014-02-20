a8faeeddd05ccf0ef062c591af95914624c61411
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-02-20 19:36:49 +0800

    community theme

--- theme/community/sidebar.php
+++ theme/community/sidebar.php
@@ -4,3 +4,10 @@
 	else $company_banner = x::url_theme().'/img/community_company_banner.png';?>
 	<img src="<?=$company_banner?>">
 </div>
+
+<div class='sidebar-banner'>
+	<?if( ms::meta('combanner_sidebar_text1') ) echo "<div class='sidebar-banner-title'>".ms::meta('combanner_sidebar_text1')."</div>"; ?>
+	<?if( ms::meta('combanner_sidebar') ) $sidebar_banner = ms::meta('img_url').ms::meta('combanner_sidebar');
+	else $sidebar_banner = x::url_theme().'/img/community_sidebar_banner.png';?>
+	<img src="<?=$sidebar_banner?>">
+</div>
