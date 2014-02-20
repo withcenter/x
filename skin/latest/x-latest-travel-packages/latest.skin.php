d3ee48ee7b017cfc2aabb26cb9a39829b603613d
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-02-20 22:14:38 +0800

    more images fixes

--- skin/latest/x-latest-travel-packages/latest.skin.php
+++ skin/latest/x-latest-travel-packages/latest.skin.php
@@ -10,20 +10,28 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 	
 		if( !$li['wr_subject'] == '' ) {
 		if( !$li['file']['count'] == 0 ) { /**checks if there is a file on the post */
-
 				$li['file']['meta'] = db::rows("SELECT * FROM $g5[board_file_table] WHERE bo_table='$bo_table' AND wr_id='".$li['wr_id']."'");
 				$li['file']['path'] = G5_URL.'/'.G5_DATA_DIR.'/file/'.$bo_table.'/';
-		}}?>
+				
+				for( $ctr = 0; $ctr <= $li['file']['count']; $ctr++ ){
+					if( $li['file']['meta'][$ctr]['bf_file'] ){
+						$has_image = $li['file']['meta'][$ctr]['bf_file'];
+						break;
+					}
+				}
+			}
+			else $has_image = null;
+		}
+		?>
 		<a href='<?=$li['href']?>'>
 		<?
 			if( $count == 5 ) $nomargin = 'no-margin';
 		?>
 			<div class='travel-package-container <?=$nomargin?>'>
 				<div class='travel-package'>
-					<div class='travel-image'>
+					<div class='travel-image'>					
 					<?
-						if ( $li['file']['meta'][0]['bf_file'] != '' || $li['file']['meta'][1]['bf_file'] != '') {
-							//$imgsrc = $li['file']['path'].$li['file']['meta'][$i]['bf_file']; 
+						if ( $has_image) {							
 							$imgsrc = get_list_thumbnail($bo_table, $li['wr_id'], 187, 104);							
 						}
 						else $imgsrc['src'] = $latest_skin_url.'/img/no-image.png';
