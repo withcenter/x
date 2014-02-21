eb6c8203c328572e652257fb842231fbcf8d7878
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-02-21 18:35:21 +0800

    Community Theme

--- skin/latest/x-latest-community-posts/latest.skin.php
+++ skin/latest/x-latest-community-posts/latest.skin.php
@@ -13,7 +13,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 						<div class='label'><?=$bo_subject?></div>
 					</td>
 					<td align='right'>
-						<div class='posts-more'><a href="<?=g::url()?>/bbs/board.php?bo_table=<?=$bo_table?>">more <img src="<?=$latest_skin_url?>/img/more-icon.png"></a></div>
+						<div class='posts-more'><a href="<?=g::url()?>/bbs/board.php?bo_table=<?=$bo_table?>" target='_blank'>more <img src="<?=$latest_skin_url?>/img/more-icon.png"></a></div>
 					</td>
 				</tr>
 			</table>
@@ -26,7 +26,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 				$subject = $li['wr_subject'];
 				$subject .= ":";
 				$content = cut_str($li['wr_content'],30,'...');
-				$url = g::url().'/bbs/board.php?bo_table='.$li['bo_table'].'&wr_id='.$li['wr_id'];
+				$url = $li['href'];
 				$comment_count = $li['wr_comment'];
 				$no_comment = '';
 				if ( !$comment_count = $li['comment_cnt'] ) {
@@ -35,7 +35,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 				}
 		?>	
 			<li>
-				<a href='<?=$url?>'>					
+				<a href='<?=$url?>' target='_blank'>					
 				<?
 					echo "<div class='subject'>$subject</div> <div class='post-content'>$content</div> <div class='no-of-comments $no_comment'>($comment_count)</div>";
 				?>
