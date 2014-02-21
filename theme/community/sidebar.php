10d6e124600a2e81ed579861b3ecbcb07bca9b95
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-02-21 23:45:35 +0900

    Community Theme

--- theme/community/sidebar.php
+++ theme/community/sidebar.php
@@ -103,11 +103,13 @@ if( $recent_comments ) {
 			$comments_content = cut_str($comments_li['wr_content'],30,'...');
 			$comments_url = g::url().'/bbs/board.php?bo_table='.$comments_li['bo_table'].'&wr_id='.$comments_li['wr_id'];
 			$comments_author = $comments_li['mb_id'];
+			
+			date_default_timezone_set("Asia/Seoul");
+			$current_time = new DateTime(date('Y-m-d H:m:s'), new DateTimeZone("Asia/Seoul"));
+			$old_time = new DateTime($comments_li['wr_datetime'], new DateTimeZone("Asia/Seoul"));
 
-			$current_time = new DateTime(date('Y-m-d H:m:s'), new DateTimeZone('Europe/Berlin'));
-			$old_time = new DateTime($comments_li['wr_datetime'], new DateTimeZone('Europe/Berlin'));
+			$interval = date_diff($old_time, $current_time);
 
-			$interval = date_diff($old_time,$current_time);
 			if ( $interval->format('%y') > 0 )  $timeago = $interval->format('%yyrs');
 			else if ( $interval->format('%m') > 0 ) $timeago = $interval->format('%mmo(s)');
 			else if ( $interval->format('%d') > 0 ) $timeago = $interval->format('%day(s)');
