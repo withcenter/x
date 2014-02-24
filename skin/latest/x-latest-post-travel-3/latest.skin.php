3f58ab8bdf0e1b9dbad29e46815c6e35eda8812e
yonggyo00 <lyonggyo@gmail.com>
2014-02-24 17:45:40 +0800

    work for travel theme translation

--- skin/latest/x-latest-post-travel-3/latest.skin.php
+++ skin/latest/x-latest-post-travel-3/latest.skin.php
@@ -1,7 +1,6 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
-<<<<<<< HEAD
 if ( $list ) {?>
 	<link rel="stylesheet" href="<?php echo $latest_skin_url ?>/style.css">
 	<?
@@ -11,42 +10,7 @@ if ( $list ) {?>
 	?>
 	<div class="image-posts" >
 		<div class='inner'>
-			<div class='title'><img src="<?=$latest_skin_url?>/img/posts_icon.png"><div class='label'><?=$bo_subject?></div></div>
-=======
-<link rel="stylesheet" href="<?php echo $latest_skin_url ?>/style.css">
-<?
-global $board;
-?>
-<div class="image-posts" >
-	<div class='inner'>
-		<div class='title'><img src="<?=$latest_skin_url?>/img/posts_icon.png"><div class='label'>Single Image Post</div></div>
-			
-				<?php
-					$num = 1;
-					foreach ( $list as $li ) {
-					if( $num == 3 ) $style = 'no-border'; 					
-					if( $board['bo_table'] )
-					$thumbnail = get_list_thumbnail($board['bo_table'], $li['wr_id'], 56, 56);
-				?>	
-					<div class='items <?=$style?>'>
-					<?
-						$subject = conv_subject($li['wr_subject'], 20, "...");
-						$content = cut_str($li['wr_content'], 200, "...");						
-						
-						$subject .= ":";
-						$url = $li['href'];
-						echo "
-							<a href='$url'>
-								<img align='left' src='".$thumbnail['src']."'/>
-								<span class='subject'>$subject</span>
-								<span class='content'>".strip_tags($content)."</span>
-							</a>
-						";
-						$num ++;
-					?>
-					</div>
->>>>>>> dd96c20c8630586bb197c3a15491de29972c11a0
-				
+			<div class='title'><img src="<?=$latest_skin_url?>/img/posts_icon.png"><div class='label'><?=$bo_subject?></div></div>				
 					<?php
 						$num = 1;
 						foreach ( $list as $li ) {
@@ -72,11 +36,7 @@ global $board;
 						?>
 						</div>
 					
-					<?}	
-						if( empty($list) ){
-							echo "empty";
-						}
-					?>	
+					<?}	?>
 			<a class='more-posts' href='<?=g::url()?>/bbs/board.php?bo_table=<?=$bo_table?>'><img src='<?=$latest_skin_url?>/img/more-btn.png'/></a>
 		</div>
 	</div>
