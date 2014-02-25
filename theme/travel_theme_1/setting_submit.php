38cb2f5ac2d11cdb264b0c87d24a6a4ad913d1a1
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-02-25 14:56:41 +0800

    travel theme, changed settings_submit to use $[in]

--- theme/travel_theme_1/setting_submit.php
+++ theme/travel_theme_1/setting_submit.php
@@ -1,6 +1,6 @@
 ﻿<?php
-	ms::meta('travelbanner_1_text1',$travelbanner_1_text1);
-	ms::meta('travelbanner_2_text1',$travelbanner_2_text1);
-	ms::meta('travelbanner_3_text1',$travelbanner_3_text1);
-	ms::meta('travelbanner_4_text1',$travelbanner_4_text1);
-	ms::meta('travelbanner_5_text1',$travelbanner_5_text1);
+	ms::meta('travelbanner_1_text1',$in['travelbanner_1_text1']);
+	ms::meta('travelbanner_2_text1',$in['travelbanner_2_text1']);
+	ms::meta('travelbanner_3_text1',$in['travelbanner_3_text1']);
+	ms::meta('travelbanner_4_text1',$in['travelbanner_4_text1']);
+	ms::meta('travelbanner_5_text1',$in['travelbanner_5_text1']);
