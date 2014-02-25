50c41bf8c96e45cc4e09515bb36facebba64fc21
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-02-25 22:44:55 +0800

    theme config on ie7

--- theme/community_2/top.php
+++ theme/community_2/top.php
@@ -4,6 +4,6 @@
 		<a href='<?=G5_BBS_URL?>/board.php?bo_table=<?=ms::board_id( etc::domain() ).'_1'?>'>자유토론</a>
 		<? if ( !login() ) { ?> <a href='<?=G5_BBS_URL?>/register.php'>회원가입</a> <? } ?>
 		<? if( ms::meta( 'com2_info' ) ) { ?>  <a href='<?=g::url()?>/?page=intro'>회사소개</a> <?}?>
-		<? if( $qna_forum = ms::meta( 'com2_info' ) ) { ?>  <a href='<?=G5_BBS_URL?>/board.php?bo_table=<?=$qna_forum?>'>고객지원</a> <?}?>
+		<? if( $qna_forum = ms::meta( 'qna_forum' ) ) { ?>  <a href='<?=G5_BBS_URL?>/board.php?bo_table=<?=$qna_forum?>'>고객지원</a> <?}?>
 	</div>
 </div>
