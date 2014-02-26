7862f9364c108cf1eb4ed063e2f78e1cd82a77f3
yonggyo00 <lyonggyo00@gmail.com>
2014-02-26 16:33:43 +0800

    fixing css problem for x-outlogin-community-2

--- skin/outlogin/x-outlogin-community-2/outlogin.skin.2.php
+++ skin/outlogin/x-outlogin-community-2/outlogin.skin.2.php
@@ -17,7 +17,7 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 		<div class='admin-mode'><a href="<?=x::url_admin()?>">X Admin Panel</a></div>
 	<?php }  ?>
      
-		<div class='user-info'><b><?php echo $nick ?></b>님 로그인&nbsp;&nbsp;
+		<div class='user-info'><b><?php echo $nick ?></b>님 로그인
 			<a href="<?php echo G5_BBS_URL ?>/memo.php" target="_blank" id="ol_after_memo" class="win_memo">
 				쪽지<span class='no_of_unreaded_message'><?php echo $memo_not_read ?></span>
 			</a>
