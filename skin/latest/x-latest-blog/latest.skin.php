2f5decba83a6ee294278b73e4db105304e83e31a
JaeHo Song <thruthesky@gmail.com>
2014-02-26 22:09:20 +0900

    updating thumbnail code

--- skin/latest/x-latest-blog/latest.skin.php
+++ skin/latest/x-latest-blog/latest.skin.php
@@ -1,7 +1,6 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
-include_once(x::dir().'/class/temp_image_resizer.php');
-include_once(G5_LIB_PATH.'/thumbnail.lib.php'); 
+
 ?>
 
 <link rel="stylesheet" href="<?php echo $latest_skin_url ?>/style.css">
