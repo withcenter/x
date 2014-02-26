e52456bb2b75fd19f7b5d8954c814dee4d5ddec0
yonggyo00 <lyonggyo00@gmail.com>
2014-02-26 18:37:27 +0800

    fixing bug

--- theme/blog/head.php
+++ theme/blog/head.php
@@ -46,6 +46,7 @@
 		<?php
 			if ( write_page() ) {
 				$ids = ms::forum();
+				di ( $ids );
 				$str = "<div class='forum-write-title'>글을 쓰려는 게시판을 선택해 주세요.</div>";
 				foreach ( $ids as $id ) {
 					
