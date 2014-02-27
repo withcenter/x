026785d9e408d5f7800146e89dd44ccb2896648f
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-02-27 22:35:47 +0800

    widget

--- skin/latest/x-community_3_images_with_caption/latest.skin.php
+++ skin/latest/x-community_3_images_with_caption/latest.skin.php
@@ -9,21 +9,6 @@ include_once(G5_LIB_PATH.'/thumbnail.lib.php');
 	if ( $list ) {
 		$count = 1;
 		foreach( $list as $li ) {
-		
-			if( !$li['wr_subject'] == '' ) {
-			if( !$li['file']['count'] == 0 ) { /**checks if there is a file on the post */
-					$li['file']['meta'] = db::rows("SELECT * FROM $g5[board_file_table] WHERE bo_table='$bo_table' AND wr_id='".$li['wr_id']."'");
-					$li['file']['path'] = G5_URL.'/'.G5_DATA_DIR.'/file/'.$bo_table.'/';
-					
-					for( $ctr = 0; $ctr <= $li['file']['count']; $ctr++ ){
-						if( $li['file']['meta'][$ctr]['bf_file'] ){
-							$has_image = $li['file']['meta'][$ctr]['bf_file'];
-							break;
-						}
-					}
-				}
-				else $has_image = null;
-			}
 			?>		
 			<?
 				//if( $count % 4 == 0 ) $nomargin = 'no-margin';
@@ -32,13 +17,11 @@ include_once(G5_LIB_PATH.'/thumbnail.lib.php');
 				<div class='community_images_with_captions_container <?=$nomargin?>'>
 					<div class='images_with_captions'>
 						<div class='caption_image'>					
-						<?
-							if ( $has_image) {							
-								$imgsrc = get_list_thumbnail($bo_table, $li['wr_id'], 172, 87);							
-							}
-							else $imgsrc['src'] = $latest_skin_url.'/img/no-image.png';
+						<?						
+							$imgsrc = get_list_thumbnail($bo_table, $li['wr_id'], 172, 87);							
 							
-							
+							if( !$imgsrc['src'] ) $imgsrc['src'] = $latest_skin_url.'/img/no-image.png';
+														
 							$img = "<img src='$imgsrc[src]'/>";						
 							echo "<div class='img-wrapper'><a href='$li[href]'>".$img."</a></div>";
 						?>
