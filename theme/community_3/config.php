fb8961c9e13059aad4d0cd79d0ae9f71bc059e56
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-02-27 14:19:13 +0800

    community_3

---
+++ theme/community_3/config.php
@@ -0,0 +1,3 @@
+<?php
+	$theme_config['name'] = "Community_3";	
+	$theme_config['type'] = "subsite";
---
+++ theme/community_3/head.php
@@ -0,0 +1,24 @@
+﻿<link rel='stylesheet' type='text/css' href='<?=x::url_theme()?>/css/theme.css' />
+<script src='<?=x::url_theme()?>/js/theme.js' /></script>
+
+
+
+
+
+
+
+
+
+
+
+
+<div class='layout'>
+	<table class='main-content' cellpadding=0 cellspacing=0 width='100%' border=0>
+		<tr valign='top'>
+			<td class='content'>
+			
+			</td>
+			<td width='10'></td>
+			<td class='right' width='200'>	
+				<? include x::theme('right') ?>
+			</td>
