064fd5d712b81b5f48e4ce56c362fb6016964f1b
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-02-28 15:09:32 +0800

    widget fixes (mostly css)

--- theme/community_3/index.php
+++ theme/community_3/index.php
@@ -37,72 +37,45 @@
 <div class='images_with_caption_wrapper'>
 <?
 $latest_bo_table = ms::board_id(etc::domain()).'_1';
-$latest_1_output = latest("x-community_3_images_with_caption", $latest_bo_table, 8, 20);
+$latest_1_output = latest("x-community_3_images_with_caption", $latest_bo_table, 4, 20);
 echo $latest_1_output;
 ?>
-</div>
-
-<div class='bulleted_list'>
-	<table width='100%' cellspacing=0 cellpadding=0><tr valign='top'>
-	<td width='50%'>
-	<?
-	$latest_bo_table = ms::board_id(etc::domain()).'_1';
-	$latest_1_output = latest("x-community-3-bulleted-list", $latest_bo_table, 14, 20);
-	echo $latest_1_output;
-	?>
-	</td>
-	<td width='50%'>
-	<?
-	$latest_bo_table = ms::board_id(etc::domain()).'_2';
-	$latest_1_output = latest("x-community-3-bulleted-list", $latest_bo_table, 14, 20);
-	echo $latest_1_output;
-	?>
-	</td>
-	</tr></table>
 </div>
 
 <div class='timed_list'>
 	<table width='100%' cellspacing=0 cellpadding=0><tr valign='top'>
 	<td class='left' width='50%'>
-	<?
-	$latest_bo_table = ms::board_id(etc::domain()).'_1';
-	$latest_1_output = latest("x-community-3-timed-list", $latest_bo_table, 11, 20);
-	echo $latest_1_output;
-	?>
+		<?
+		$latest_bo_table = ms::board_id(etc::domain()).'_1';
+		$latest_1_output = latest("x-community-3-timed-list", $latest_bo_table, 5, 25);
+		echo $latest_1_output;
+		?>
 	</td>
 	<td class='right' width='50%'>
-	<?
-	$latest_bo_table = ms::board_id(etc::domain()).'_2';
-	$latest_1_output = latest("x-community-3-timed-list", $latest_bo_table, 11, 20);
-	echo $latest_1_output;
-	?>
+		<?
+		$latest_bo_table = ms::board_id(etc::domain()).'_2';
+		$latest_1_output = latest("x-community-3-timed-list", $latest_bo_table, 5, 25);
+		echo $latest_1_output;
+		?>
 	</td>
 	</tr></table>
 </div>
 
-<div class='images_with_caption_2'>
-	<?
-	$latest_bo_table = ms::board_id(etc::domain()).'_1';
-	$latest_1_output = latest("x-community_3_images_with_caption_2", $latest_bo_table, 7, 20);
-	echo $latest_1_output;
-	?>
-</div>
-
-<div class='timed_list'>
+<div class='timed_list_with_images'>
 	<table width='100%' cellspacing=0 cellpadding=0><tr valign='top'>
 	<td width='50%'>
-	<?
-	$latest_bo_table = ms::board_id(etc::domain()).'_1';
-	$latest_1_output = latest("x-community-3-timed-list-with-images", $latest_bo_table, 6, 20);
-	echo $latest_1_output;
-	?>
+		<?
+		$latest_bo_table = ms::board_id(etc::domain()).'_1';
+		$latest_1_output = latest("x-community-3-timed-list-with-images", $latest_bo_table, 4, 50);
+		echo $latest_1_output;
+		?>
 	</td>
 	<td width='50%' class='right'>
-	<?
-	$latest_bo_table = ms::board_id(etc::domain()).'_2';
-	$latest_1_output = latest("x-community-3-timed-list-with-images", $latest_bo_table, 6, 20);
-	echo $latest_1_output;
-	?>
+		<?
+		$latest_bo_table = ms::board_id(etc::domain()).'_2';
+		$latest_1_output = latest("x-community-3-timed-list-with-images", $latest_bo_table, 4, 50);
+		echo $latest_1_output;
+		?>
 	</td>
 	</tr></table>
 </div>
