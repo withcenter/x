93ee5836214a724ae3fd039181414579233e7ca1
yonggyo00 <lyonggyo@gmail.com>
2014-02-28 13:12:44 +0800

    fixing x-community_3_images_with_caption

--- skin/latest/x-community_3_images_with_caption/latest.skin.php
+++ skin/latest/x-community_3_images_with_caption/latest.skin.php
@@ -8,27 +8,13 @@ include_once(G5_LIB_PATH.'/thumbnail.lib.php');
 <?php
 	if ( $list ) {
 		foreach( $list as $li ) {
-<<<<<<< HEAD
-		?>
-=======
-			?>		
-			<?
-				//if( $count % 4 == 0 ) $nomargin = 'no-margin';
-				//else $nomargin = null;
-			?>
->>>>>>> 8c68cad4cfc3635978b42db9021ac13a91b27484
+?>
 				<div class='community_images_with_captions_container <?=$nomargin?>'>
 					<div class='images_with_captions'>
 						<div class='caption_image'>					
 						<?						
 							$imgsrc = get_list_thumbnail($bo_table, $li['wr_id'], 172, 87);							
-<<<<<<< HEAD
 							if ( empty($imgsrc['src']) )  $imgsrc['src'] = $latest_skin_url.'/img/no-image.png';
-							
-=======
->>>>>>> 8c68cad4cfc3635978b42db9021ac13a91b27484
-							
-							if( !$imgsrc['src'] ) $imgsrc['src'] = $latest_skin_url.'/img/no-image.png';
 														
 							$img = "<img src='$imgsrc[src]'/>";						
 							echo "<div class='img-wrapper'><a href='$li[href]'>".$img."</a></div>";
