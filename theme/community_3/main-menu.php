167a06175273766fdfa364c15df47fc629d6c164
yonggyo00 <lyonggyo@gmail.com>
2014-03-02 01:36:52 +0900

    move admin menu to right

--- theme/community_3/main-menu.php
+++ theme/community_3/main-menu.php
@@ -2,12 +2,7 @@
 	
 	<?
 		if( ms::admin() ) {
-		$max_menus = 6;
-	?>
-			<li class="comm3_menu" page = "admin-menu">
-				<a href="<?=ms::url_config()?>">사이트 관리</a>
-			</li>
-	<?
+			$max_menus = 6;
 		}
 		else $max_menus = 7;
 	?>
@@ -23,6 +18,14 @@
 		<li class="comm3_menu <?=$no_margin?>" page = "<?=ms::meta('menu_'.$i)?>"><a href='<?=g::url()?>/bbs/board.php?bo_table=<?=ms::meta('menu_'.$i)?>'><?=$menu?></a></li>
 	<?}}?>
 
+	<?
+		if ( ms::admin() ) {
+	?>
+	
+			<li class="comm3_menu" page = "admin-menu">
+				<a href="<?=ms::url_config()?>">사이트 관리</a>
+			</li>
+	<? } ?>
 	
 	
 </ul>
