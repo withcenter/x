9439182c2f6680cbb2ff4a1748f68358fc1b2087
yonggyo00 <lyonggyo@gmail.com>
2014-03-03 22:26:36 +0800

    fixing travel theme2

--- skin/latest/x-latest-travel-lower-posts/latest.skin.php
+++ skin/latest/x-latest-travel-lower-posts/latest.skin.php
@@ -9,7 +9,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
     <div class='travel_lower_post_title'>
 		<?if( $options ) echo "<img class='icon' src='".$options."'/>";?>
 		<span class='board_subject'><?php echo cut_str( $bo_subject, 20, "..." );?></span>
-		<a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=<?php echo $bo_table ?>">MORE</a>
+		<a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=<?php echo $bo_table ?>">자세히</a>
 	</div>
 	<div class='travel_lower_items'>
     <?php for ($i=0; $i<count($list); $i++) { 
@@ -22,14 +22,34 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 				else $comments = $list[$i]['comment_cnt'];							
             ?>
 			<div class='subject'>
-			<img class='bullet' src='<?=$latest_skin_url?>/img/arrow-bullet.png' />
-			<a href='<?=$list[$i]['href']?>'><?=$list[$i]['subject']?></a>
-			<div class='comments'>[<?=$comments?>]</div>
+				<img class='bullet' src='<?=$latest_skin_url?>/img/arrow-bullet.png' />
+				<a href='<?=$list[$i]['href']?>'><?=$list[$i]['subject']?></a>
+				<div class='comments'>[<?=$comments?>]</div>
 			</div>			
 		</div>
     <?php }  ?>
     <?php if (count($list) == 0) { //게시물이 없을 때  ?>
-    <div>게시물이 없습니다.</div>
+		<div class='item'>
+			<div class='subject'>
+				<img class='bullet' src='<?=$latest_skin_url?>/img/arrow-bullet.png' />
+				<a href='http://www.philgo.net/bbs/board.php?bo_table=help&wr_id=5'>사이트 만들기 안내</a>
+				<div class='comments'>[5]</div>
+			</div>
+		</div>
+		<div class='item'>
+			<div class='subject'>
+				<img class='bullet' src='<?=$latest_skin_url?>/img/arrow-bullet.png' />
+				<a href='http://www.philgo.net/bbs/board.php?bo_table=help&wr_id=5'>블로그 만들기</a>
+				<div class='comments'>[5]</div>
+			</div>
+		</div>
+		<div class='item'>
+			<div class='subject'>
+				<img class='bullet' src='<?=$latest_skin_url?>/img/arrow-bullet.png' />
+				<a href='http://www.philgo.net/bbs/board.php?bo_table=help&wr_id=5'  style='color: #cc4235; font-weight: bold;'>여행사 사이트 만들기</a>
+				<div class='comments'>[5]</div>
+			</div>	
+		</div>
     <?php }  ?>
 	</div>   
 </div>
