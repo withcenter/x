bb719fa23d514efd50ce30ace95fea9fc403b32e
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-03-03 19:10:42 +0800

    css

--- theme/travel_theme_2/index.php
+++ theme/travel_theme_2/index.php
@@ -10,19 +10,21 @@
 			<?
 				if ( $has_images ) {
 					$banner_url = ms::meta('img_url');
+					$banner_num = 1;
 					for ( $i = 1; $i <= 5 ; $i++) {
 						if( !$banner_image = ms::meta( 'travel2banner_'.$i ) ){
 							continue;
 						}
 						if ( $i == 1 ) $first_image = 'selected';
 						else $first_image = '';
-						echo "<div class='banner-image image_num_$i $first_image'>";
+						echo "<div class='banner-image image_num_$banner_num $first_image'>";
 						if ( !$url = ms::meta('travel2banner_'.$i.'_text2') )  $url = "javascript:void(0)";
 						$thumb = g::thumbnail_from_image_tag( "<img src='".$banner_url.$banner_image."'>", ms::board_id( etc::domain() ).'_1', 748, 238 );
 						echo "<a href='$url' target='_blank'><img src='".$thumb."'>";
 						echo "<p class='banner-text'><span class='banner-content'>".cut_str(strip_tags(ms::meta('travel2banner_'.$i.'_text1')),60,'...')."</span></p>";
 						echo "<div class='banner-more'>자세히</div>";
 						echo "</a></div>";
+						$banner_num++;
 					}
 				}
 				else {
