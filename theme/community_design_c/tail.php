f93da7bede0487d8c4eda2667d38828c6bcacaca
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-03-03 20:23:44 +0800

    community design 3

---
+++ theme/community_design_c/tail.php
@@ -0,0 +1,19 @@
+﻿
+			</div><!--content-->			
+		
+		</div><!--main-content-->
+	</div><!--body-wrapper-->
+	<div style='clear:both'></div>
+	<div class='footer'>		
+			<div class='copyright'>
+				<div class='inner'>					
+						<div class='text-info'>						
+							<? 
+								if ( $footer_text = ms::meta('footer_text') ) echo $footer_text; 
+								else echo "어드민 페이지에서 하단 문구를 입력해 주세요";
+							?>
+						</div>
+				</div>
+			</div>
+		</div><!--footer-->
+</div><!--layout-->
