06caa38dda6a7a02bde1a66008b2124f36ab140c
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-03-04 18:17:32 +0800

    css fixes with IE 7

--- theme/community_4/head.php
+++ theme/community_4/head.php
@@ -61,66 +61,92 @@
 		</div>
 	</div>
 	<div class='header'>
-	<table width='100%' cellpadding=0 cellspacing=0><tr valign='top'>
-		<td width=400>
-			<div id="comm3_logo">
-				<a href="<?php echo G5_URL ?>">
-				<?if( ms::meta('header_logo') ) { ?>
-					<img src="<?=ms::meta('img_url').ms::meta('header_logo')?>">
-				<?} else {?>
-					<img src='<?=x::url_theme()?>/img/banner.png'>
-				<?}?>
-				</a>
-			</div>
-		</td>
-		<td>
-			<fieldset id="search_field">
-				<legend>사이트 내 전체검색</legend>
-				<form name="comm3_search_form" method="get" action="<?php echo G5_BBS_URL ?>/search.php" onsubmit="return fsearchbox_submit(this);">
-				<input type="hidden" name="sfl" value="wr_subject||wr_content">
-				<input type="hidden" name="sop" value="and">
-				<label for="sch_stx" class="sound_only">검색어<strong class="sound_only"> 필수</strong></label>
-					<table id='search_table_holder' cellpadding=0 cellspacing=0><tr>
-						<td>
-							<input type="text" name="stx" id="comm3_search_text" maxlength="20" placeholder='Search' autocomplete='off'>
-						</td>
-						<td width=40>				
-							<input type="image" src="<?=x::url_theme()?>/img/search2.png" id="comm3_search_submit">
-						</td>
-					</tr></table>
-				</form>
+		<div class='header_and_logo'>
+			<table width='100%' cellpadding=0 cellspacing=0><tr valign='top'>
+				<td width=400>
+					<div id="comm3_logo">
+						<a href="<?php echo G5_URL ?>">
+						<?if( ms::meta('header_logo') ) { ?>
+							<img src="<?=ms::meta('img_url').ms::meta('header_logo')?>">
+						<?} else {?>
+							<img src='<?=x::url_theme()?>/img/banner.png'>
+						<?}?>
+						</a>
+					</div>
+				</td>
+				<td>
+					<fieldset id="search_field">
+						<legend>사이트 내 전체검색</legend>
+						<form name="comm3_search_form" method="get" action="<?php echo G5_BBS_URL ?>/search.php" onsubmit="return fsearchbox_submit(this);">
+						<input type="hidden" name="sfl" value="wr_subject||wr_content">
+						<input type="hidden" name="sop" value="and">
+						<label for="sch_stx" class="sound_only">검색어<strong class="sound_only"> 필수</strong></label>
+							<table id='search_table_holder' cellpadding=0 cellspacing=0><tr>
+								<td>
+									<input type="text" name="stx" id="comm3_search_text" maxlength="20" placeholder='Search' autocomplete='off'>
+								</td>
+								<td width=40>				
+									<input type="image" src="<?=x::url_theme()?>/img/search2.png" id="comm3_search_submit">
+								</td>
+							</tr></table>
+						</form>
 
-				<script>
-				function fsearchbox_submit(f)
-				{
-					if (f.stx.value.length < 2) {
-						alert("검색어는 두글자 이상 입력하십시오.");
-						f.stx.select();
-						f.stx.focus();
-						return false;
-					}
+						<script>
+						function fsearchbox_submit(f)
+						{
+							if (f.stx.value.length < 2) {
+								alert("검색어는 두글자 이상 입력하십시오.");
+								f.stx.select();
+								f.stx.focus();
+								return false;
+							}
 
-					// 검색에 많은 부하가 걸리는 경우 이 주석을 제거하세요.
-					var cnt = 0;
-					for (var i=0; i<f.stx.value.length; i++) {
-						if (f.stx.value.charAt(i) == ' ')
-							cnt++;
-					}
+							// 검색에 많은 부하가 걸리는 경우 이 주석을 제거하세요.
+							var cnt = 0;
+							for (var i=0; i<f.stx.value.length; i++) {
+								if (f.stx.value.charAt(i) == ' ')
+									cnt++;
+							}
 
-					if (cnt > 1) {
-						alert("빠른 검색을 위하여 검색어에 공백은 한개만 입력할 수 있습니다.");
-						f.stx.select();
-						f.stx.focus();
-						return false;
-					}
+							if (cnt > 1) {
+								alert("빠른 검색을 위하여 검색어에 공백은 한개만 입력할 수 있습니다.");
+								f.stx.select();
+								f.stx.focus();
+								return false;
+							}
 
-					return true;
-				}
-				</script>
-			</fieldset>
-		<td>		
-		</tr></table>
-	</div>
+							return true;
+						}
+						</script>
+					</fieldset>
+				</td>		
+			</tr></table>
+		</div><!--logo and search-->
+	</div><!--header-->
+	
+<!--[if IE 7]>
+	<style>
+		.top .inner{
+			width:970px;
+		}
+		
+				
+		.top .inner a{
+				height:19px;
+		}			
+		
+		.header .header_and_logo #comm3_logo{
+			width: 400px;
+			margin-top:7px;
+		}
+		
+		
+	</style>
+<![endif]-->	
+
+
+
+
 	<div class='main-menu'>	
 			<?include x::theme('main-menu')?>				
 	</div>	
