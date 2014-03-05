a9114192592f1e76cd372577b711d8098e4014a3
JaeHo Song <thruthesky@gmail.com>
2014-03-05 17:00:47 +0800

    skin update: let users to update skin information right on the browser.

--- module/skin/update.php
+++ module/skin/update.php
@@ -4,6 +4,10 @@
 		x::config("bo_table.$code", $bo_table);
 		x::config("bo_subject.$code", $bo_subject);
 		x::config("css.$code", $_POST['css'] );
+		if ( $submit_value == 'Submit & Close' ) {
+			echo "<script>parent.location.reload();</script>";
+			exit;
+		}
 	}
 	
 
@@ -24,14 +28,19 @@ var g5_url       = "<?php echo G5_URL ?>";
 </head>
 <body>
 
+<div class='layout'>
+
+<div class='page-title'>Skin Update</div>
 
 <? if ( $submit ) { ?>
 <div class='submitted'>Skin Information Updated !</div>
 <? } ?>
-code : <?=$code?>
-<br>
 
 
+<!--<span class='caption'>Code</span> : <?=$code?><br>-->
+
+
+<div class='content'>
 <form action='?' method='POST'>
 <input type='hidden' name='module' value='<?=$module?>'>
 <input type='hidden' name='action' value='<?=$action?>'>
@@ -39,14 +48,41 @@ code : <?=$code?>
 <input type='hidden' name='theme' value='n'>
 <input type='hidden' name='submit' value='1'>
 
-Forum ID : <input type='text' name='bo_table' value="<?=x::config("bo_table.$code")?>"> ( bo_table )<br>
-Forum Title : <input type='text' name='bo_subject' value="<?=x::config("bo_subject.$code")?>"> ( bo_subject )<br>
-STYLE ( CSS )<br>
-<textarea class='css' name='css' ><?=htmlspecialchars_decode (x::config("css.$code"))?></textarea><br>
+<span class='caption'>Forum ID</span> : 
+<?
+	if ( ms::exist() ) {
+		$cfgs = ms::forums();
+		if ( ! empty( $cfgs ) ) {
+?>
+<select name='select_bo_table'>
+	<option value=''>Select Forum ID</option>
+	<? foreach ( $cfgs as $c ) { ?>
+		<option value="<?=$c['bo_table']?>"><?=$c['bo_subject']?></option>
+	<? } ?>
+</select>
+<script>
+$(function(){
+	$("[name='select_bo_table']").change(function(){
+		$("[name='bo_table']").val($(this).val());
+	});
+});
+</script>
+<?
+		}
+	}
+?>
 
+<input type='text' name='bo_table' value="<?=x::config("bo_table.$code")?>" placeholder=" Input bo_table ex) qna"><br>
+<span class='caption'>Forum Title</span> : <input type='text' name='bo_subject' value="<?=x::config("bo_subject.$code")?>" placeholder=" Input Forum Subject"><br>
+<hr>
+<span class='caption'>STYLE ( CSS )</span><br>
+<textarea class='css' name='css' ><?=htmlspecialchars_decode (x::config("css.$code"))?></textarea><br>
+</div>
 <input type='submit'>
+<input type='submit' name='submit_value' value='Submit & Close'>
 
 
 </form>
+</div>
 </body>
 </html>
