fbc5418ee5988503acd3072fbc5d365827dd416c
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-03-05 15:08:32 +0800

    CSS Tweaks

--- theme/community_2/tail.php
+++ theme/community_2/tail.php
@@ -2,7 +2,7 @@
 		</tr>
 	</table>
 	<div class='footer'><div class='inner'>
-		<? if( $footer_text = ms::meta('footer_text') ) echo $footer_text;
+		<? if( $footer_text = ms::meta('footer_text') ) echo nl2br($footer_text);
 			else echo "COPYRIGHT (C) 2014&nbsp;&nbsp;&nbsp;&nbsp;ALL RIGHT RESERVED&nbsp;&nbsp;&nbsp;&nbsp;WITHCENTER.COM";
 		?>
 	</div></div>
