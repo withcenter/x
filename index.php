c91c81b6537a4e9c80e1b6d43ba9e75bf9b8503d
JaeHo Song <thruthesky@gmail.com>
2014-03-06 19:11:38 +0900

    commenting on start script

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <?php
-define('_INDEX_', true);
-include_once('../common.php');
-include_once(G5_PATH.'/head.php');
-if ( preg_match('/^admin_/', $action) ) include x::admin_menu();
-if ( ! empty( $module ) ) {
-	include module( 'init' );
-	include module( $action );
-}
-else {
-	echo "module is empty. GO TO Admin Page <a href='?module=admin&action=index'>ADMIN Page</a><br>";
-	//include "xml_sample/demo/client.php";
-	//include "etc/test/post.php";
-}
-include_once(G5_PATH.'/tail.php');
+/**
+ * @file index.php
+ * @desc X start script.
+ */
+	define('_INDEX_', true);
+	include_once('../common.php');
+	include_once(G5_PATH.'/head.php');
+	
+	if ( preg_match('/^admin_/', $action) ) include x::admin_menu();
+	if ( ! empty( $module ) ) {
+		include module( 'init' );
+		include module( $action );
+	}
+	else {
+		echo "module is empty. GO TO Admin Page <a href='?module=admin&action=index'>ADMIN Page</a><br>";
+		//include "xml_sample/demo/client.php";
+		//include "etc/test/post.php";
+	}
+	include_once(G5_PATH.'/tail.php');
 
+	/** end of start script */
