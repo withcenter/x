fc0fc77ba3bbccaa8e41353dc9512f07b5d7ddfb
JaeHo Song <thruthesky@gmail.com>
2014-03-08 05:57:29 +0800

    bug fix on visit (local)

--- etc/hook/vist.lib.before-skin.php
+++ etc/hook/vist.lib.before-skin.php
@@ -5,10 +5,12 @@
 	
 	$value = x::config( "visit." . etc::domain() );
 	$visit = unserialize( $value );
+
 	
 	
 	
 	$path = x::dir() . "/skin/visit/" . $skin_dir;
+	
 	if ( file_exists( $path ) ) {
 		$visit_skin_path = $path;
 		$visit_skin_url = x::url() . "/skin/visit/" . $skin_dir;
