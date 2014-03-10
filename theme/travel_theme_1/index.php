03f619f0ef9b00e241164b946dc5c0ba56ecf655
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-03-10 17:38:59 +0800

    CSS Tweaks

--- theme/travel_theme_1/index.php
+++ theme/travel_theme_1/index.php
@@ -20,7 +20,7 @@
 								echo "<div class='banner-image image_num_$banner_num $first_image'>";
 								if ( !$url = ms::meta('travel2banner_'.$i.'_text2') )  $url = "javascript:void(0)";
 								echo "<a href='$url' target='_blank'><img src='".$banner_url.$banner_image."'></a>";
-								echo "<p class='banner-text'><span class='banner-content'>".cut_str(strip_tags(ms::meta('travel2banner_'.$i.'_text1')),54,'...')."</span></p>";
+								echo "<span class='banner-content'><p class='banner-text'>".cut_str(strip_tags(ms::meta('travel2banner_'.$i.'_text1')),54,'...')."</p></span>";
 								echo "<div class='banner-more'><a href='$url' target='_blank'>자세히</a></div>";
 								echo "</div>";
 								$banner_num++;
