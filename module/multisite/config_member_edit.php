1d1be2be29494b392fb5f778a87a0424ec9e7c4f
yonggyo00 <lyonggyo@gmail.com>
2014-03-11 14:23:09 +0800

    config_member.php

--- module/multisite/config_member_edit.php
+++ module/multisite/config_member_edit.php
@@ -41,54 +41,81 @@ if ( $in['done'] ) {
 $minfo = db::row("SELECT * FROM ".$g5['member_table']." WHERE mb_id='$mb_id'");
 ?>
 <form method='get'>
-	<div class='member-info-edit'>
+	<div class='member-info'>
 		<input type='hidden' name='module' value='multisite' />
 		<input type='hidden' name='action' value='config_member' />
 		<input type='hidden' name='option' value='mb_edit' />
 		<input type='hidden' name='mb_id' value='<?=$mb_id?>' />
 		<input type='hidden' name='done' value=1 />
-		<div>
 			<div class='basic-info'>
+				<div class='title'>기본정보</div>
 				<div><span class='sub-title'>아이디</span> <?=$minfo['mb_id']?></div>
 				<div><span class='sub-title'>가입일</span> <?=$minfo['mb_open_date']?></div>
-				<div><span class='sub-title'>마지막 로그인 날짜</span> <?=$minfo['mb_today_login']?></div>
+				<div><span class='sub-title'>로그인 날짜</span> <?=$minfo['mb_today_login']?></div>
 				<div><span class='sub-title'>로그인 아이피</span> <?=$minfo['mb_login_ip']?></div>
 			</div>
-			<div class='member-info-edit'>회원정보 수정</div>
+
 			<div class='member-info-content-edit'>
-				<div><span class='sub-title'>이름</span> <input type='text' name='mb_name' value='<?=$minfo['mb_name']?>' /></div>
-				
-				<div><span class='sub-title'>닉네임</span> <input type='text' name='mb_nick' value='<?=$minfo['mb_nick']?>' /></div>
-				<div><span class='sub-title'>이메일</span> <input type='text' name='mb_email' value='<?=$minfo['mb_email']?>' /></div>
-				<div><span class='sub-title'>홈페이지</span> <input type='text' name='mb_homepage' value='<?=$minfo['mb_homepage']?>' /></div>
-				
-				<div><span class='sub-title'>성별</span> <input type='text' name='mb_sex' value='<?=$minfo['mb_sex']?>' /></div>
-				
-				
-				<div><span class='sub-title'>유선전화</span> <input type='text' name='mb_tel' value='<?=$minfo['mb_tel']?>' /></div>
-				
-				<div><span class='sub-title'>휴대전화</span> <input type='text' name='mb_hp' value='<?=$minfo['mb_hp']?>' /></div>
-				
-				<div>
-					<span class='sub-title'>우편번호</span> 
-					<input type='text' name='mb_zip1' value='<?=$minfo['mb_zip1']?>' /> - <input type='text' name='mb_zip2' value='<?=$minfo['mb_zip2']?>' />
-				</div>
-				<div>
-					<span class='sub-title'>주소</span> 
-					<input type='text' name='mb_addr1' value='<?=$minfo['mb_addr1']?>' /> <input type='text' name='mb_addr2' value='<?=$minfo['mb_addr2']?>' />
-					<div><input type='text' name='mb_addr3' value='<?=$minfo['mb_addr3']?>' /></div>
-					<div><input type='text' name='mb_addr_jibeon' value='<?=$minfo['mb_addr_jibeon']?>' /></div>
-				</div>
-				<div><span class='sub-title'>서명</span> <textarea name='mb_name'><?=$minfo['mb_signature']?></textarea></div>
-				
-				<div><span class='sub-title'>자기소개</span> <textarea name='mb_name'><?=$minfo['mb_profile']?></textarea></div>
-				
-				
-			</div>
+				<div class='title'>회원정보 수정</div>
+				<table cellpadding=0 cellspacing=0 width='100%'>
+					<tr>
+						<td width=120><span class='sub-title'>이름</span></td>
+						<td> <input type='text' name='mb_name' value='<?=$minfo['mb_name']?>' /></td>
+					</tr>
+					<tr>
+						<td width=120><span class='sub-title'>닉네임</span></td>
+						<td><input type='text' name='mb_nick' value='<?=$minfo['mb_nick']?>' /></td>
+					</tr>
+					<tr>
+						<td width=120><span class='sub-title'>이메일</span></td>
+						<td><input type='text' name='mb_email' value='<?=$minfo['mb_email']?>' /></td>
+					</tr>
+					<tr>
+						<td width=120><span class='sub-title'>홈페이지</span></td> 
+						<td><input type='text' name='mb_homepage' value='<?=$minfo['mb_homepage']?>' /></td>
+					</tr>
+					<tr>
+						<td width=120><span class='sub-title'>성별</span></td>
+						<td><input type='text' name='mb_sex' value='<?=$minfo['mb_sex']?>' /></td>
+					</tr>
+					
+					<tr>
+						<td width=120><span class='sub-title'>유선전화</span></td> 
+						<td><input type='text' name='mb_tel' value='<?=$minfo['mb_tel']?>' /></td>
+					</tr>
+					<tr>
+						<td width=120><span class='sub-title'>휴대전화</span></td> 
+						<td><input type='text' name='mb_hp' value='<?=$minfo['mb_hp']?>' /></td>
+					</tr>
+					<tr>
+						<td width=120><span class='sub-title'>우편번호</span></td>
+						<td>
+							 <input type='text' name='mb_zip1' value='<?=$minfo['mb_zip1']?>' /> - <input type='text' name='mb_zip2' value='<?=$minfo['mb_zip2']?>' />
+						</td>
+					</tr>
+					<tr>
+						<td width=120>
+							<span class='sub-title'>주소</span> 
+						</td>
+						<td>
+							<input type='text' name='mb_addr1' value='<?=$minfo['mb_addr1']?>' /> <input type='text' name='mb_addr2' value='<?=$minfo['mb_addr2']?>' />
+							<div><input type='text' name='mb_addr3' value='<?=$minfo['mb_addr3']?>' /></div>
+							<div><input type='text' name='mb_addr_jibeon' value='<?=$minfo['mb_addr_jibeon']?>' /></div>
+						</td>
+					</tr>
+					<tr>
+						<td width=120><span class='sub-title'>서명</span></td>
+						<td><textarea name='mb_name'><?=$minfo['mb_signature']?></textarea></td>
+					</tr>
+					<tr>
+						<td width=120><span class='sub-title'>자기소개</td> 
+						<td><textarea name='mb_name'><?=$minfo['mb_profile']?></textarea></td>
+					</tr>
+			</table>
 		</div>
 		<div>
-			<div class='member-control'>회원 통제</div>
 			<div class='member-control'>
+				<div class='title'>회원 통제</div>
 				<div><span class='sub-title'>포인트</span> <input type='text' name='mb_hp' value='<?=$minfo['mb_point']?>' /></div>
 				<div><span class='sub-title'>레벨</span> <input type='text' name='mb_level' value='<?=$minfo['mb_level']?>' /></div>
 				<div><span class='sub-title'>비밀번호 변경</span> <input type='text' name='mb_password' /></div>
