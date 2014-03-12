9d19c541e02bf5b1f3d042f018c66f182d32cfda
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-03-12 22:57:02 +0800

    Admin Link x-outlogin-blog

--- skin/outlogin/x-outlogin-blog/outlogin.skin.2.php
+++ skin/outlogin/x-outlogin-blog/outlogin.skin.2.php
@@ -18,36 +18,24 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 			<img src='<?=$outlogin_skin_url?>/lock2.png'/> 비밀번호 변경
 		</a>
 	</div>
-	<div class='logout-button'>
-		<a href="<?php echo G5_BBS_URL ?>/logout.php">로그아웃</a>
+	<div class='middle-panel'>
+	<?php if ($is_admin == 'super' || $is_auth) {  ?>
+	<div class='admin-mode'>
+		<div><a href="<?php echo G5_ADMIN_URL ?>">ADMIN PANEL</a></div>
+		<div><a href="<?=x::url_admin()?>">X Admin Panel</a></div>
+	</div>
+	<?php }  ?>
+	<div class='logout-button' <?if ($is_admin == 'super' || $is_auth) echo "style='float: right; width: 82px;'"?>>
+		<a href="<?php echo G5_BBS_URL ?>/logout.php" <?if ($is_admin == 'super' || $is_auth) echo "style='padding: 0;'"?>>로그아웃</a>
+	</div>
+		<div style='clear: both'></div>
 	</div>
 	<div class='account-settings'>
 		<a href="<?php echo G5_BBS_URL ?>/member_confirm.php?url=register_form.php">
 			<img src='<?=$outlogin_skin_url?>/setting.png'/> 회원정보변경
 		</a>
 	</div>
-<!--		<div class='login-box-info'>
-			<?php if ($is_admin == 'super' || $is_auth) {  ?>
-				<div class='admin-mode'><a href="<?php echo G5_ADMIN_URL ?>">ADMIN PANEL</a></div>
-				<div class='admin-mode'><a href="<?=x::url_admin()?>">X Admin Panel</a></div>
-			<?php }  ?>
-			<div class='username'><?php echo $nick ?><?=lang(' is logged in', '님 로그인')?></div>
-			
-			<div class='setting'>
-				<img src='<?=$outlogin_skin_url?>/message.png' />
-				<a href="<?php echo G5_BBS_URL ?>/memo.php" target="_blank" id="ol_after_memo" class="win_memo">쪽지
-					<span style='border-radius: 30%; padding: 0 0.5em; font-size: 11px; color: #000000; background-color: #ffffff;' class='message_new'><?=$memo_not_read ?></span>
-				</a>
-			</div>
-			
-			<div class='profile'>
-				<img src='<?=$outlogin_skin_url?>/lock.png' />
-				<a href="<?php echo G5_BBS_URL ?>/member_confirm.php?url=register_form.php" id="ol_after_info">정보수정</a>
-			</div>
-			
-			<span class='logout-button'>
-			<a href="<?php echo G5_BBS_URL ?>/logout.php" id="ol_after_logout">로그아웃</a></span>
-		</div> -->
+
 		
 </div></div>
 
