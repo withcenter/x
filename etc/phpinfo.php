db8c6b4884a62990e8bc74bc566212d03e332c3c
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-03-12 19:58:53 +0800

    adding phpinfo()

--- etc/phpinfo.php
+++ etc/phpinfo.php
@@ -2,34 +2,6 @@
 /**
  * @edited again by jaeho at 8:47 pm 2014-03-12
  */
-echo "<h1>PHPINFO()</h1>";
 
-// edited by ben
-echo "<hr>";
-
-
-// first edited by jaeho
 phpinfo();
 
-
-echo "edited by ben - 7:51pm PH";
-
-
-// edited by yonggyo lee
-
-// edited by yonggyo lee
-
-// edited once more
-
-
-// by ben
-
-
-echo "by ben: again on 7:35pm ph";
-
-echo "by ben: again on 7:35pm ph";
-
-
-
-// by ben 2
-echo "by ben 2 " . date( "Ymd-his");
