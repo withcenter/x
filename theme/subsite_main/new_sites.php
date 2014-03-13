81a69f72db839ecc84f90af6ec4c130462e1bb82
yonggyo00 <lyonggyo@gmail.com>
2014-03-13 17:55:50 +0800

    fixing popular_forums

--- theme/subsite_main/new_sites.php
+++ theme/subsite_main/new_sites.php
@@ -2,18 +2,18 @@
 	<div class='header'><img src='<?=x::url_theme()?>/img/bars.png'/>신규 사이트</div>
 		<div class='inner'>
 		<?
-		$q = "SELECT * FROM x_multisite_config ORDER BY stamp_create DESC LIMIT 3";
+		$q = "SELECT domain, mb_id FROM x_site_config ORDER BY idx DESC LIMIT 3";
 		$queries = db::rows($q);
 		//di($queries);
 		foreach ( $queries as $query ) {
-		if( $query['title'] == '' ) $query['title'] = 'No Title';				
-		
-		$site_url = ms::url_site($query['domain']);
+			if ( !$title = x::meta_get( $query['domain'], 'title' ) )  $title = '제목 없음';
+			
+			$site_url = ms::url_site($query['domain']);
 		?>
 		<div class='info-wrapper'>
 			<div class='top-info site-domain'><a href='<?=$site_url?>' target='_blank'><?=$query['domain']?></a></div>
 			<div class='other-info site-owner'><a href='<?=$site_url?>' target='_blank'>만든이: <?=$query['mb_id']?></a></div>
-			<div class='other-info site-title'><a href='<?=$site_url?>' target='_blank'><?=$query['title']?></a></div>
+			<div class='other-info site-title'><a href='<?=$site_url?>' target='_blank'><?=$title?></a></div>
 		</div>
 		<?}?>		
 	</div>
--- theme/subsite_main/popular_forums.php
+++ theme/subsite_main/popular_forums.php
@@ -1,27 +1,23 @@
 <div class='left-menu-wrapper'>
 	<div class='header'><img src='<?=x::url_theme()?>/img/star.png'/>죄회수가 많은 글</div>
 		<div class='inner'>
-		<?
-		$q = "SELECT * FROM `x_post_data`";
-		$popular_forums = db::rows($q);
-		foreach ( $popular_forums as $key => $value ) {
-			$popular_forums[$key]['bo_subject']	= db::result("SELECT bo_subject FROM ".$g5['board_table']." WHERE bo_table='".$value['bo_table']."'");
-		}
-		function comp_hit($a, $b) {
-			return $b['wr_hit'] - $a['wr_hit'];
-		}
-		usort($popular_forums, 'comp_hit');
-
-		for( $count = 0; $count < 3; $count++ ) {
-		$post_url = g::url()."/bbs/board.php?bo_table=".$popular_forums[$count]['bo_table']."&wr_id=".$popular_forums[$count]['wr_id'];
-	
-		$popular_subject = cut_str($popular_forums[$count]['wr_subject'], 15, "...");
-		$popular_content = cut_str($popular_forums[$count]['wr_content'], 20, "...");
-		?>
+		<?php
+		$posts = g::posts(
+			array(
+				'limit'					=> 3,
+				'order by' => 'wr_hit DESC'
+			)
+		);
+			$i = 0;
+			foreach ( $posts as $p ) {
+				$subject = cut_str(strip_tags($p['wr_subject']), 15, "...");
+				$content = cut_str(strip_tags($p['wr_content']), 20, "...");
+				$post_url = ms::url_site($p['domain'])."/bbs/board.php?bo_table=".$p['bo_table']."&wr_id=".$p['wr_id'];
+			?>
 			<div class='info-wrapper'>
-				<div class='top-info bo-table'><a href='<?=$post_url?>' target='_blank'><?=$count+1?>) <?=$popular_forums[$count]['bo_subject']?> (조회수: <?=$popular_forums[$count]['wr_hit']?> )</a></div>
-				<div class='other-info subject'><a href='<?=$post_url?>' target='_blank'><?=strip_tags($popular_subject)?></a></div>
-				<div class='other-info content'><a href='<?=$post_url?>' target='_blank'><?=strip_tags($popular_content)?></a></div>
+				<div class='top-info bo-table'><a href='<?=$post_url?>' target='_blank'><?=++$i?>) <?=$p['domain']?> (조회수: <?=number_format($p['wr_hit'])?> )</a></div>
+				<div class='other-info subject'><a href='<?=$post_url?>' target='_blank'><?=$subject?></a></div>
+				<div class='other-info content'><a href='<?=$post_url?>' target='_blank'><?=$content?></a></div>
 			</div>
 		<?}?>
 
