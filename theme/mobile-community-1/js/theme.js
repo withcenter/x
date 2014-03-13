71f4a991cddf84b34c36cb48416dbaba61d94253
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-03-13 21:15:00 +0800

    main menu mobile theme

--- theme/mobile-community-1/js/theme.js
+++ theme/mobile-community-1/js/theme.js
@@ -2,6 +2,15 @@ $(function(){
 	$('#login-button').click(function(){
 		if ( $('#login-box').css('display') == 'none' ) $('#login-box').show();
 		else $('#login-box').hide();
+	});	
+
+	$('#header .menu ul li.menu_item a').click(function(){	
+		$('#header .menu ul li.menu_item a').removeClass('selected');
+		$(this).addClass('selected');				
+	});
+	
+	$('#header .menu ul li.log-in-button a').click(function(){		
+		$('.pop-up-login').toggle();		
 	});
 });
 
