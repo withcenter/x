8c3ba0d9c9ef98c554459d1e656304340ad15ff9
arvindomingo <arvin.o.domingo@gmail.com>
2014-03-13 07:44:38 +0100

    Popular Forums

--- theme/subsite_main/popular_forums.php
+++ theme/subsite_main/popular_forums.php
@@ -2,45 +2,28 @@
 	<div class='header'><img src='<?=x::url_theme()?>/img/star.png'/>죄회수가 많은 글</div>
 		<div class='inner'>
 		<?
-		$q = "SELECT bo_table, bo_subject FROM ".$g5['board_table'];
-		$queries = db::rows($q);
-	if ( $queries ) {
-		$allhits = Array();
-		$i = 0;
-
-		foreach ( $queries as $query ){
-			$q2 = "SELECT wr_id, wr_subject, wr_content, wr_hit FROM ".$g5['write_prefix'].$query['bo_table'];
-			$queries2 = db::rows($q2);
-	
-			foreach( $queries2 as $qs2 ){
-				$qs2['bo_table'] = $query['bo_table'];
-				$qs2['bo_subject'] = $query['bo_subject'];
-				$allhits[$i] = $qs2;
-				$i++;
-			}					
+		$q = "SELECT * FROM `x_post_data`";
+		$popular_forums = db::rows($q);
+		foreach ( $popular_forums as $key => $value ) {
+			$popular_forums[$key]['bo_subject']	= db::result("SELECT bo_subject FROM ".$g5['board_table']." WHERE bo_table='".$value['bo_table']."'");
 		}
-
-		/** @todo : bug check 
-			@warning : do not use anonymous function.
-		*/
 		function comp_hit($a, $b) {
 			return $b['wr_hit'] - $a['wr_hit'];
 		}
-		usort($allhits, 'comp_hit');
+		usort($popular_forums, 'comp_hit');
 
 		for( $count = 0; $count < 3; $count++ ) {
-		$post_url = g::url()."/bbs/board.php?bo_table=".$allhits[$count]['bo_table']."&wr_id=".$allhits[$count]['wr_id'];
-		
-		$popular_subject = cut_str($allhits[$count]['wr_subject'], 15, "...");
-		$popular_content = cut_str($allhits[$count]['wr_content'], 20, "...");
+		$post_url = g::url()."/bbs/board.php?bo_table=".$popular_forums[$count]['bo_table']."&wr_id=".$popular_forums[$count]['wr_id'];
+	
+		$popular_subject = cut_str($popular_forums[$count]['wr_subject'], 15, "...");
+		$popular_content = cut_str($popular_forums[$count]['wr_content'], 20, "...");
 		?>
 			<div class='info-wrapper'>
-				<div class='top-info bo-table'><a href='<?=$post_url?>' target='_blank'><?=$count+1?>) <?=$allhits[$count]['bo_subject']?> (조회수: <?=$allhits[$count]['wr_hit']?> )</a></div>
+				<div class='top-info bo-table'><a href='<?=$post_url?>' target='_blank'><?=$count+1?>) <?=$popular_forums[$count]['bo_subject']?> (조회수: <?=$popular_forums[$count]['wr_hit']?> )</a></div>
 				<div class='other-info subject'><a href='<?=$post_url?>' target='_blank'><?=strip_tags($popular_subject)?></a></div>
 				<div class='other-info content'><a href='<?=$post_url?>' target='_blank'><?=strip_tags($popular_content)?></a></div>
 			</div>
 		<?}?>
-	<? }?>	
-	</div>
+
 	<div class='view-more'><a href="<?=x::url()?>/?module=multi&action=forum.list">자세히</a></div>
 </div>
