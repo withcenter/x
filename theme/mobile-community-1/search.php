0c0c46adadacccfb9b37e0d772453046d5b1cfa5
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-03-14 19:55:21 +0800

    search box fix (made it working)

--- theme/mobile-community-1/search.php
+++ theme/mobile-community-1/search.php
@@ -1,5 +1,5 @@
-<form>
-
-<input class='key' type='text' name='key' autocomplete='off'><input class='submit' type='image' src='<?=x::url_theme()?>/img/search_icon.png'>
-
+<form name="fsearchbox" method="get" action="<?php echo G5_BBS_URL ?>/search.php" onsubmit="return fsearchbox_submit(this);">
+	<input type="hidden" name="sfl" value="wr_subject||wr_content">
+	<input type="hidden" name="sop" value="and">
+	<input class='key' type='text' name="stx" autocomplete='off'><input class='submit' type='image' src='<?=x::url_theme()?>/img/search_icon.png'>
 </form>
