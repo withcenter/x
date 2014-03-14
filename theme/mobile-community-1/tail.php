d34a19b592037e93e6354b81c0c47d074bd609c1
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-03-14 19:34:56 +0800

    reponsive footer

--- theme/mobile-community-1/tail.php
+++ theme/mobile-community-1/tail.php
@@ -19,15 +19,26 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 
 <div id='footer'>
+<table width='100%' cellspacing=0 cellpadding=0></tr valign='top'>
+<td>
 	<div class='logo'>
 		<img src='<?=x::url_theme()?>/img/footerlogo.png'/>
 	</div>
+	<div class='logo-640px'>
+		<img src='<?=x::url_theme()?>/img/footer-640px.png'/>
+	</div>
+	<div class='logo-500px'>
+		<img src='<?=x::url_theme()?>/img/footer-500px.png'/>
+	</div>
+</td>
+<td>
 	<div class='text-info'>
 		<div class='footer-text'><?=nl2br(x::meta('footer_text'))?></div>		
 		<a href="<?=g::url()?>?device=pc">PC 버젼</a>
 		<a href='<?=url_site_config()?>'>사이트 관리</a>
-	</div>
-	<div style='clear:both;'></div>
+	</div>	
+</td>
+</tr></table>
 </div>
 </body>
 </html>
