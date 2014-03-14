55d385598382afa100e081c6e43ad5656ef61e21
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-03-14 19:08:37 +0800

    Popular Posts

--- theme/mobile-community-1/latest_posts.php
+++ theme/mobile-community-1/latest_posts.php
@@ -2,14 +2,3 @@
 	<img src='<?=x::url_theme()?>/img/latest_post_icon.png'/>최근글
 </div>
 
-<div class='latest_posts'>
-<?php
-	echo latest( 'x-rwd-gallery-2-mobile-latest-posts', bo_table(1) , 40, 40, 1,
-		array(
-			'width'		=> 214,
-			'height'	=> 170,
-			'radius'	=> 0,
-		)
-	);
-?>
-</div>
