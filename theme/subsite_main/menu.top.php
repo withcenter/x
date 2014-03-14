b8ffe32abe3e4b9425ed851dbfbfa6c122f91f6f
yonggyo00 <lyonggyo@gmail.com>
2014-03-14 14:24:54 +0800

    migrating multisite to multi

--- theme/subsite_main/menu.top.php
+++ theme/subsite_main/menu.top.php
@@ -13,7 +13,7 @@
 		if ( $is_admin == 'super' ){
 	?>
 	<li class="menu-list">
-		<a href="<?=url_site_config()?>" class="admin">사이트관리</a>
+		<a href="<?=x::url()?>/?module=admin&action=index" class="admin">사이트관리</a>
 	</li>
 	<?}?>
 	
@@ -22,21 +22,9 @@
 	<? }?>
 	
 	<? if ( $member['mb_id'] ) {?>
-		<li class="menu-list"><a class='my-sites' href='<?=x::url()?>/?module=multisite&action=my_sites'>내 사이트</a></li>
+		<li class="menu-list"><a class='my-sites' href='<?=x::url()?>/?module=multi&action=my_sites'>내 사이트</a></li>
 	<? }?>
 	
-	<? /*
-	<? for ( $i = 1; $i <= 10; $i++ ) { ?>
-	
-	<? if ( $extra['menu_'.$i] != '' ) {
-		$option = db::row("SELECT bo_subject FROM $g5[board_table] WHERE bo_table = '".$extra['menu_'.$i]."'");
-	
-		if ( $extra['submenu_'.$i] ) $menu_id = "menu=$i";
-		else $menu_id = null;		
-	?>
-		<li class="menu-list" <?=$menu_id?>><a class='<?php echo $extra['menu_'.$i]?>' href='<?=g::url()?>/bbs/board.php?bo_table=<?=$extra['menu_'.$i]?>' class="menu-list" <?// if ($extra['menu'.$i.'_target'] == 'Y') echo "target='_blank'"?>><?=$option['bo_subject']?></a></li>				
-	<?}}?>
-	*/?>
 	
 	<li class="menu-list">
 		<a href="http://philgo.com" target='_blank' class="main-site">필고</a>
