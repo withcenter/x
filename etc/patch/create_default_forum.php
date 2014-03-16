a7366fcd0f41ecd6049bf2552a234b1fc1ccf6f3
JaeHo Song <thruthesky@gmail.com>
2014-03-17 00:46:30 +0800

    patch on install scripts

--- etc/patch/create_default_forum.php
+++ etc/patch/create_default_forum.php
@@ -1,4 +1,28 @@
 <?php
+
+	/** @short 아래의 코드는 웹 브라우저로 그누보드5 & Extended 처음 설치시에 같이 설치가 되도록 하기 위한 코드이다.
+	 *
+	 *
+	 */
+	include_once "$dir_root/x/class/etc.php";
+	include_once "$dir_root/x/class/database.php";
+	include_once "$dir_root/x/class/x.php";
+	include_once "$dir_root/x/class/gnuboard.php";
+	
+	if ( ! function_exists( 'patch_message' ) ) {
+		function patch_message()
+		{
+		}
+	}
+	if ( empty($g5['member_table']) ) {
+		$g5['write_prefix']		= $table_prefix;
+		$g5['group_table']		= $table_prefix . "group";
+		$g5['board_table']		= $table_prefix . "board";
+	}
+	/** eo */
+
+
+
 	if ( g::group_exist('multisite') ) {
 		patch_message("multisite group already created");
 	}
@@ -13,5 +37,3 @@
 		g::board_create( array('id'=>'default', 'subject'=>'Default Forum', 'group_id'=>'multisite') );
 		patch_message("default  forum created");
 	}
-	
-	
