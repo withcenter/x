ccf88d960de4f019229453d155fd4f2be515370e
JaeHo Song <thruthesky@gmail.com>
2014-03-19 15:11:46 +0800

    patch on default theme for minor update

--- theme/default/menu.php
+++ theme/default/menu.php
@@ -1,8 +1,6 @@
 <?php
 	
-	list ( $main, $subs ) = g::get_menu();
-
-
+	
 ?>
 
    <nav id="gnb">
@@ -11,32 +9,11 @@
 <script>$('#gnb').addClass('gnb_js');</script>
 <h2>메인메뉴</h2>
 <ul id="gnb_1dul">
-	<?php
-		$i = 0;
-		foreach ( $main as $row ) {
-			
-	 ?>
+	
 	<li class="gnb_1dli">
-		<a href="<?php echo G5_BBS_URL ?>/group.php?gr_id=<?php echo $row['gr_id'] ?>" class="gnb_1da"><?php echo $row['gr_subject'] ?></a>
-		<ul class="gnb_2dul">
-			<?php
-				$sub = $subs[$i++];
-				
-					foreach ( $sub as $row2 ) {
-			 ?>
-				<li class="gnb_2dli"><a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=<?php echo $row2['bo_table'] ?>" class="gnb_2da"><?php echo $row2['bo_subject'] ?></a></li>
-			<?php
-					}
-			?>
-		</ul>
-		
+		<a href="<?php echo G5_URL ?>" class="gnb_2da">홈</a>
 	</li>
-	<?php } ?>
-	
-	
-	
-	
-	
+
 </ul>
     
     </nav>
