660991f9a5049687e305fc7a5e2752a3043f722c
JaeHo Song <thruthesky@gmail.com>
2014-03-19 19:56:41 +0900

    patching on member skin. moving ob_start() before extend script in common.php

--- etc/begin.php
+++ etc/begin.php
@@ -20,56 +20,6 @@
 	 *
 	 */
 	if ( preg_match("/^admin_/i", $action) && ! super_admin() ) { jsBack("관리자가 아닙니다."); exit; }
-	
-	
-	
-// -----------------------------------------------------------------------------
-//
-// @TODO ordered by JaeHo
-// 1. FIND A BETTER PLACE FOR THIS HOOK. 'x/begin.php' IS NOT A GOOD PLACE FOR HOOK.
-// 2. Do not let admin to input CSS or Javascript.
-// 3. Do not use anonymous function. it produces error on PHP 5.2 and below.
-//
-/** first if: display sidebar to left or right based on the multisite admin settings
-	second if: attach custom css based on the multisit admin settings
- */
-
-/** 1. Moved the Hook to init.php of Theme Folder
-	2. Removed Custom CSS Textarea on config_global.php
-*/
-
-//Multisite Config Options
-// $extra = ms::get_extra( );
-/** @short load 'register_result.skin.php' in the 'x/theme/xxxx/' folder instead of 'skin/' folder.
- *  
- */
-if ( strpos($_SERVER['PHP_SELF'], 'register_result.php') !== false && file_exists(x::theme('register_result.skin')) ) {
-	$member_skin_path	= x::theme_folder();
-	$member_skin_url	= x::theme_url(  );
-}
-/** @short the code below are the same as above.
- * 
-if ( strpos($_SERVER['PHP_SELF'], 'register_result.php') !== false ) {
-	
-	if (isset($_SESSION['ss_mb_reg']))
-		$mb = get_member($_SESSION['ss_mb_reg']);
-
-	// 회원정보가 없다면 초기 페이지로 이동
-	if (!$mb['mb_id'])
-		goto_url(G5_URL);
-
-	$g5['title'] = '회원가입이 완료되었습니다.';
-	include_once('./_head.php');
-	$path = x::theme('register_result.skin');
-	if ( file_exists( $path ) ) {
-		include_once( $path );
-	}
-	else include_once($member_skin_path.'/register_result.skin.php');
-	include_once('./_tail.php');
-	exit;
-}
-*/
-
 
 /** @short Reset board skin path for PC and mobile.
  *  -----------------------------------------------
@@ -92,7 +42,6 @@ if ( strpos( $board_skin_path, "board/basic" ) ) {
 }
 
 
-
 /** @short RESET MEMBER SKIN for PC and Mobile
  *  -----------------------------------------------
  *  
@@ -104,12 +53,12 @@ $member_skin_path = str_replace("skin/", 'x/skin/', $member_skin_path);
 
 $member_skin_url = str_replace("mobile/", '', $member_skin_url);
 $member_skin_url = str_replace("skin/", 'x/skin/', $member_skin_url);
-
-
-
 $member_skin = x::$config['site']['member_skin'];
+
 $member_skin_path = substr_replace( $member_skin_path, "/member/$member_skin", strpos($member_skin_path, '/member/') );
 $member_skin_url = substr_replace( $member_skin_url, "/member/$member_skin", strpos($member_skin_url, '/member/') );
+
+	
 
 
 
