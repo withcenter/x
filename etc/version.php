f9c764c6d70c175fed006f28839bfdd1b8f70428
JaeHo Song <thruthesky@gmail.com>
2014-03-21 02:27:58 +0800

    update alert feature.

--- etc/version.php
+++ etc/version.php
@@ -1,12 +1,22 @@
 <?php
-$version = "2014.03.20.0";
+$version = "2014.03.21.0";
 $version_message=<<<EOH
+<html>
+<head>
+<meta charset="utf-8">
 	<style>
 		body {
 			margin: 0;
 			padding: 0;
 			background-color: #efefef;
+			font-size: 9pt;
 		}
 	</style>
-	새로운 버젼이 있습니다. 업데이트를 하십시오.
+</head>
+<body>
+	<div>
+	새로운 버젼 (<span class='version'>$version</span>) 이 있습니다. 관리자 페이지에서 자동 업데이트를 하십시오.
+	</div>
+</body>
+</html>
 EOH;
