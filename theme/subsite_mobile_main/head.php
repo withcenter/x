259245987d94ca821bca7a6cb6cf4a68679b4ae6
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-03-21 15:03:47 +0800

    tablet and mobile header

--- theme/subsite_mobile_main/head.php
+++ theme/subsite_mobile_main/head.php
@@ -6,10 +6,16 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 <div class ='layout'>
 	<div class='header'>
 		<div class='inner'>
-			<div class='logo'>
-				<a href='<?=G5_URL?>'><img src='<?=x::url_theme()?>/img/logo.png'/></a>
+			<div class='logo-wrapper'>
+				<div class='logo'><a href='<?=G5_URL?>'><img src='<?=x::url_theme()?>/img/logo.png'/></a></div>
+				<div class='text-info tablet'>
+					<span class='info call-us'>Call us! 070-7529-1749 /</span>
+					<span class='info text'>질문과 답변 /</span>
+					<span class='info text'>로그인 /</span>
+					<span class='info text'>회원가입 /</span>
+				</div>
 			</div>
-			<div class='right_menus'>
+			<div class='right_main_menus above-400px'>
 				<ul>
 					<li><div class='extra-border'></div><a href='javascript:void(0)'>Site Quotation</a></li>
 					<li><div class='extra-border'></div><a href='javascript:void(0)'>Logo Banner Quotation</a></li>
@@ -19,13 +25,23 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 				<div style='clear:both;'></div>
 			</div>
 			<div style='clear:both;'></div>
-			<div class='text-info'>
+			<div class='text-info web-browser'>
 				<span class='info call-us'>Call us! 070-7529-1749 /</span>
 				<span class='info text'>질문과 답변 /</span>
 				<span class='info text'>로그인 /</span>
 				<span class='info text'>회원가입 /</span>
+			</div>			
+		</div>		
+			<div class='right_main_menus below-400px'>
+				<ul>
+					<li><div class='extra-border'></div><a href='javascript:void(0)'>Site Quotation</a></li>
+					<li><div class='extra-border'></div><a href='javascript:void(0)'>Logo Banner Quotation</a></li>
+					<li><div class='extra-border'></div><a href='javascript:void(0)'>Site Gallery</a></li>
+					<li><div class='extra-border'></div><a href='javascript:void(0)'>Template Gallery</a></li>							
+				</ul>						
+				<div style='clear:both;'></div>
 			</div>
-		</div>
+			<div style='clear:both;'></div>
 	</div>
 	
 	<div class='container'>
