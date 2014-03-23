00ef923df57154ef3543ca2ff138242ccca4d7e7
JaeHo Song <thruthesky@gmail.com>
2014-03-23 18:55:48 +0800

    update module

--- module/update/admin_theme.php
+++ module/update/admin_theme.php
@@ -1,13 +1,5 @@
 <link rel="stylesheet" href="<?=module("$module.css")?>">
-
-테마를 추가, 삭제 또는 업데이트 합니다.<br>
-
-&nbsp;<br>
-
-
-업데이트 가능한 테마 | <a href='?module=update&action=admin_theme_installed'>추가된 테마</a> | 추가되어 있지 않은 테마<br>
-
-
+<?include 'menu_theme.php'?>
 
 <div class='list'>
 <iframe src="<?=URL_X?>/?module=update&action=list&mode=theme_list_updatable&theme=n"></iframe>
