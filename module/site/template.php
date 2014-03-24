5fa64308021b431cc07f8b25d9ff23d8f41d2a3c
yonggyo00 <lyonggyo@gmail.com>
2014-03-24 13:58:16 +0800

    template.php

--- module/site/template.php
+++ module/site/template.php
@@ -7,58 +7,21 @@
 		$dirs = file::getDirs(X_DIR_THEME);
 		foreach ( $dirs as $dir ){
 			if ( $dir == $active_theme ) {
-<<<<<<< HEAD
-				$theme_config = array();
-				include X_DIR_THEME . "/$dir/config.php";
-				$active_theme_name = $theme_config['name'];
-=======
 				$theme_config = load_config( X_DIR_THEME . "/$dir/config.xml" );
-				$active_theme = $theme_config['name'];
->>>>>>> 6f43c47b74ad7a48b01dc89435ece2cd951a7091
+				$active_theme = $theme_config['name']['ko'];
+				
+				$active_theme_dir = $dir;
 				break;
 			}
 		}
 	?>
-	<?if ( $active_theme_name ) { ?>
+	<?if ( $active_theme_dir ) { ?>
 		<div class='theme-thumb'>
-			<img src='theme/<?=$active_theme?>/preview.jpg' >					
-			<div class='theme-name active-theme'><?=$active_theme_name?><span class='active-note'>가 선택 되었습니다</span></div>
+			<img src='theme/<?=$active_theme_dir?>/preview.jpg' >					
+			<div class='theme-name active-theme'><?=$active_theme?><span class='active-note'>가 선택 되었습니다</span></div>
 		</div>
 	<?}?>
-<<<<<<< HEAD
 	<div class='theme-selection'>
-		<?php
-			$theme_ctr = 2;
-				
-				foreach ( $dirs as $dir ) {
-					$path = X_DIR_THEME . "/$dir/config.php";
-					if ( ! file_exists($path) ) continue;
-					
-					$theme_config = array();
-					include $path;
-					$name = $theme_config['name'];
-					if ( empty($name) ) continue;
-					
-					$type = explode(',', $theme_config['type']);
-					
-					
-					
-					if ( ! in_array( 'pc', $type ) ) continue;
-					
-					
-					
-					
-				$url = x::url().'/theme/'.$dir.'/preview.jpg';
-				?>
-				<div class='theme-thumb inactive' theme_value='<?=$dir?>' theme_name ='<?=$theme_config['name']?>'>
-					<img src='<?=$url?>' >
-					<span class='theme-name'><?=$name?></span>
-				</div>
-				
-			<?}?>
-			<div style='clear:both'></div>
-	</div>
-=======
 	<?php
 		$theme_ctr = 2;
 			
@@ -68,7 +31,7 @@
 				
 				$theme_config = load_config($path);
 				//include $path;
-				$name = $theme_config['name'];
+				$name = $theme_config['name']['ko'];
 				if ( empty($name) ) continue;
 				
 				$type = explode(',', $theme_config['type']);
@@ -82,12 +45,11 @@
 				
 			$url = x::url().'/theme/'.$dir.'/preview.jpg';
 			?>
-			<div class='theme-thumb inactive' theme_value='<?=$dir?>' theme_name ='<?=$theme_config['name']?>'>
+			<div class='theme-thumb inactive' theme_value='<?=$dir?>' theme_name ='<?=$theme_config['name']['ko']?>'>
 				<img src='<?=$url?>' >
 				<span class='theme-name'><?=$name?></span>
 			</div>
 			
 		<?}?>
 		<div style='clear:both'></div>
->>>>>>> 6f43c47b74ad7a48b01dc89435ece2cd951a7091
 </div>			
