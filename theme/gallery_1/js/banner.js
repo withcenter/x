34096a5914877ca6c18c6b87de8c1db9901a5b67
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-03-24 19:24:42 +0800

    CSS Tweaks

--- theme/gallery_1/js/banner.js
+++ theme/gallery_1/js/banner.js
@@ -4,7 +4,7 @@ $(function(){
 	
 	var banner_rotate_interval = setInterval(function(){
 		rotate_the_banner();		
-	},5000);
+	},1000);
 	
 	$('.banner').mouseenter(function(){
 		clearInterval(banner_rotate_interval);
@@ -13,14 +13,28 @@ $(function(){
 	$('.banner').mouseleave(function(){
 		banner_rotate_interval = setInterval(function(){
 			rotate_the_banner();		
-		},5000);
+		},1000);
+	});
+	
+	$('.page_num').click(function() {
+		banner_num = $(this).attr('page_num');
+		clicked_banner( banner_num );
 	});
 	
 	function rotate_the_banner(){			
-		$('.banner-image').removeClass('selected');	
+		$('.banner-image-container').removeClass('selected');	
 		if( banner_num == no_of_images ) banner_num = 0;
 		banner_num++;
-		var banner_page = ".image_num_"+banner_num;	
+		var banner_page = ".image_"+banner_num;	
 		$(banner_page).addClass('selected');
+		$('.page_num').removeClass('selected_num');
+		$('.page_'+banner_num).addClass('selected_num');
+	}
+	
+	function clicked_banner( banner_num ) {
+		$('.banner-image-container').removeClass('selected');
+		$('.page_num').removeClass('selected_num');
+		$('.image_'+banner_num).addClass('selected');
+		$('.page_'+banner_num).addClass('selected_num');
 	}
 });
