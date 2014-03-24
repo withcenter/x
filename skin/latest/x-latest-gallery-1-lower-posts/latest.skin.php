dd52dd621cf9734dd6c5b8e526cd371b36dcd527
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-03-24 18:51:20 +0800

    Gallery Theme 1

---
+++ skin/latest/x-latest-gallery-1-lower-posts/latest.skin.php
@@ -0,0 +1,38 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+if ( ! $GLOBALS[$latest_skin_url] ++ ) { ?><link rel="stylesheet" href="<?php echo $latest_skin_url ?>/style.css"><?}?>
+
+<? if ($list) { ?>
+<div class='latest-gallery-1-lower-posts'>
+	<?	
+		$i = 1;
+		foreach ($list as $post) {
+			$thumb = get_list_thumbnail($bo_table, $post['wr_id'], 233, 208);    					            
+			if($thumb['src']) {
+				$img = '<img class="img_left" src="'.$thumb['src'].'">';
+				$count_image ++;
+			} 
+			elseif ( $image_from_tag = g::thumbnail_from_image_tag( $post['wr_content'], $bo_table, 233-2, 208-1 )) {
+				$img = "<img class='img_left' src='$image_from_tag'/>";
+				$count_image ++;
+			}
+			else {
+				$img = '<img class="img_left" src="'.$latest_skin_url.'/img/no-image.png"/>';
+				$no_image = g::thumbnail_from_image_tag( $img, $bo_table, 233-2, 208-1 );
+				$img = "<img class='img_left' src='$no_image'/>";
+			}
+	?>
+		<div class="post-item <? if ($i==1) echo 'first-item'?>">
+			<div class='post-image'>
+				<a href="<?=$post['href']?>"><?=$img?></a>
+			</div>
+			<div class='post-content-container'>
+				<div class='post-subject'><a href="<?=$post['href']?>"><?=cut_str($post['wr_subject'],20,'...')?></a></div>
+				<div class='post-content'><a href="<?=$post['href']?>"><?=cut_str(strip_tags($post['wr_content']),175,'...')?></a></div>
+			</div>
+			<div style='clear: left'></div>		
+		</div>
+	<?$i++;}?>
+		<div style='clear: left'></div>
+</div>
+<? } ?>
---
+++ theme/gallery_1/index.php
@@ -0,0 +1,56 @@
+<?
+	
+		$banners = array();
+		$container_width = 0;
+		for ( $i = 1; $i <= 5 ; $i++) { 
+			if ( file_exists( x::path_file( "banner$i" ) ) ) {
+				$banners[] = array(
+					'src' => x::url_file( "banner$i" ),
+					'href' => x::meta( "banner{$i}_url" ),
+					'subject' => x::meta("banner{$i}_subject"),
+					'content' => x::meta("banner{$i}_content")
+				);
+			}
+		}
+		$total_banners = count($banners);
+	?>	
+		<div class='banner' total_banners="<?=$total_banners?>" >
+			<div class='inner' style="position: relative;">
+				<?
+					
+					if ( $banners ) {
+						$selected = 0;
+						foreach ( $banners as $banner ) {					
+							if ( ! $selected ++ ) $first_image = 'selected';
+							else $first_image = '';
+							
+							if ( !$url = $banner['href'] ) $url = "javascript:void(0)";
+							echo "<div class='banner-image-container image_$selected $first_image' image_num='$selected'><div class='banner-image'>";
+							echo "<a href='$url' target='_blank'><img src='$banner[src]''></a></div>";
+							echo "<div class='banner-content-container'><a href='$url' target='_blank'><span class='banner-content'><p class='banner-text'><div class='banner-subject'>".cut_str(strip_tags($banner['subject']),20,'...')."</div><div class='banner-inner-contents'>".cut_str(strip_tags($banner['content']),200,'...')."</div></p></span></a>";
+							echo "<div class='banner-more'><a href='$url' target='_blank'>??? &gt;</a></div>";
+							echo "</div></div>";						
+						}
+					}
+					else {
+						echo "<img src='".x::url_theme()."/img/no_image_banner1.png' />";
+					}
+					echo "<div style='clear: left'></div>";
+				
+				?>
+			</div>
+			<div class='banner-pagination'>
+				<? for($i=1; $i<=$total_banners; $i++) {?>
+					<a href="javascript:void(0)" class='page_num page_<?=$i?> <? if ($i==1) echo "selected_num"?>' page_num='<?=$i?>'><?=$i?></a>
+				<?}?>				
+			</div>
+		</div>
+
+
+<div class='gallery-1-posts-with-image'>
+	<?=latest('x-latest-gallery-1-posts-with-image', bo_table(2), 4, 20)?>
+</div>
+
+<div class='gallery-1-lower-posts'>
+	<?=latest('x-latest-gallery-1-lower-posts', bo_table(5), 2, 20)?>
+</div>
