9080f37640d5485ad15a13ba511d9628cf6592aa
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-03-24 22:41:23 +0800

    js changes

--- theme/gallery_2/js/banner.js
+++ theme/gallery_2/js/banner.js
@@ -1,18 +1,13 @@
 $(function(){
 	var banner_rotator = setInterval( function() { rotate_the_banner() } , 5000 );
-	var banner_num, left_value, page_num, total_banners = $(".banner").attr("total_banners");
+	var banner_num=1, left_value, page_num, total_banners = $(".banner").attr("total_banners");
 	
 	/*banner rotation*/
 	function rotate_the_banner() {
-		$(".banner-image.selected").removeClass('selected').next().addClass('selected');
-		$(".page_num.selected_num").removeClass('selected_num').next().addClass('selected_num');
-			
-		if( banner_num==total_banners ) {
-			$(".banner-image").first().addClass('selected'); 
-			$(".page_num").first().addClass('selected_num');
-		}
- 
-		banner_num = $(".banner-image.selected").attr('image_num');
+		if( banner_num == total_banners ) banner_num = 0;
+		banner_num++;
+		$(".banner-image").removeClass('selected').siblings('.image_'+banner_num).addClass('selected');
+		$(".page_num").removeClass('selected_num').siblings(".page_"+banner_num).addClass('selected_num');
 		left_value = -(((banner_num) * 968));
 		if ( banner_num == 1 ) $(".images-container").css('left',0);
 		$(".images-container").animate({left: left_value}, 500);
@@ -36,13 +31,13 @@ $(function(){
 	/*pagination click()*/
 	$(".page_num").click(function(){
 		page_num = $(this).attr('page_num');
-		current_num = $(".banner-pagination").find(".page_num.selected_num").attr("page_num");
+		current_num = $(".banner-pagination").find(".selected_num").attr("page_num");
 		if ( page_num != current_num ) clicked_banner( page_num );
 	});
 	
 	/*previous button*/
 	$(".previous-banner").click(function() {
-		page_num = $(".banner-pagination").find(".page_num.selected_num").attr("page_num");
+		page_num = $(".banner-pagination").find(".selected_num").attr("page_num");
 		clicked_banner( page_num-1 );
 	});
 	
@@ -59,9 +54,9 @@ $(function(){
 		
 		$('.banner-content').fadeOut();
 		$(".images-container").animate({left: -((page_num) * 968)}, 500).find('.banner-content').fadeIn();
-		banner_num = $(".banner-image.selected").attr('image_num');
-		
+
 		previous_next_toggle( page_num );
+		banner_num = page_num;
 	}
 	
 	/*previous and next button ( show and hide ) conditions*/
