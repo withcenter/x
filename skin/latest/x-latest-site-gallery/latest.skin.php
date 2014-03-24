3bb382e4b60e77ec19ccb4e7a69b06e67b4f2c34
yonggyo00 <lyonggyo@gmail.com>
2014-03-24 18:10:48 +0800

    list skin

--- skin/latest/x-latest-site-gallery/latest.skin.php
+++ skin/latest/x-latest-site-gallery/latest.skin.php
@@ -1,8 +1,9 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
-add_stylesheet('<link rel="stylesheet" href="<?php echo $latest_skin_url ?>/style.css">', 0);
-include_once(G5_LIB_PATH.'/thumbnail.lib.php'); 
 
+add_stylesheet("<link rel='stylesheet' href='{$latest_skin_url}/style.css'>", 0);
+include_once(G5_LIB_PATH.'/thumbnail.lib.php'); 
+echo "<div class='x-latest-site-gallery'>";
 foreach ( $list as $li ) {
 	$thumb = get_list_thumbnail($bo_table, $li['wr_id'], 60, 60);
 	echo "
@@ -11,4 +12,7 @@ foreach ( $list as $li ) {
 		</div>
 	";
 };
+echo "
+		<div style='clear:left;'></div>
+	</div>";
 ?>
