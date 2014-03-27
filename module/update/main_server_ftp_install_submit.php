8455af3a0d8aab52575f08c82fe08f7ac4c3d4ab
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-03-27 21:40:33 +0800

    main_server_ftp_install_submit.php

--- module/update/main_server_ftp_install_submit.php
+++ module/update/main_server_ftp_install_submit.php
@@ -43,7 +43,7 @@ unlink ( $theme_file_path );
 
 
 //FTP file/directory upload/transfer
-include_once('phpseclib/NET/SFTP.php');
+include_once('phpseclib/Net/SFTP.php');
 include_once('phpseclib/Math/BigInteger.php');
 include_once('phpseclib/Crypt/Random.php');
 include_once('phpseclib/Crypt/Hash.php');
