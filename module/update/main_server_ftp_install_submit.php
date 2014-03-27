91cbb98caafbebc4836551fe67658da9d809e603
yonggyo00 <lyonggyo@gmail.com>
2014-03-27 21:33:54 +0800

    main_server_ftp_install.php

--- module/update/main_server_ftp_install_submit.php
+++ module/update/main_server_ftp_install_submit.php
@@ -4,11 +4,29 @@ $git_zip_host_url = "https://codeload.github.com";
 $url = "$git_zip_host_url$pu[path]/zip/master";
 
 $data_path = G5_PATH.'/data/tmp/';
+if ( is_dir ( $data_path.$project_name ) file::delete_folder ( $data_path.$project_name );
+
 $theme_file_path = $data_path.$project_name.'.zip';
 
 // file download and save to data/tmp
-$theme_file = file_get_contents($url);
-file_put_contents($theme_file_path, $theme_file);
+//$theme_file = file_get_contents($url);
+//file_put_contents($theme_file_path, $theme_file);
+// read remote file
+$file = fopen($url, 'rb');
+$content = null;
+
+while ( !feof ( $file ) ) {
+	$content .= fread($file, 512);
+}
+fclose($file);
+
+
+// write data from remote file
+$file = fopen ( $theme_file_path, 'wb');
+fwrite ( $content );
+fclose ( $file );
+
+
 
 // extract
 $zip = new ZipArchive;
