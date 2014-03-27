4bfc344810fb33edbd79c12872c1cdf941f102e7
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-03-27 16:50:57 +0800

    Sample for File Download, Extract and Rename

--- module/update/main_server_ftp_install_submit.php
+++ module/update/main_server_ftp_install_submit.php
@@ -1,42 +1,49 @@
 <?php
-
-
 $pu = parse_url( $source_link );
 $git_zip_host_url = "https://codeload.github.com";
 $url = "$git_zip_host_url$pu[path]/zip/master";
 
-
-$file = file_get_contents($url);
 $data_path = G5_PATH.'/data/tmp/';
+$theme_file_path = $data_path.$project_name.'.zip';
 
 // file download and save to data/tmp
-$file_path = $data_path.$project_name.'.zip';
-file_put_contents($file_path, $file);
+$theme_file = file_get_contents($url);
+file_put_contents($theme_file_path, $theme_file);
 
 // extract
 $zip = new ZipArchive;
-if ($zip->open($file_path) == TRUE) {
+if ($zip->open($theme_file_path) == TRUE) {
 	$zip->extractTo($data_path);
 	$zip->close();
 	echo 'File Extracted';
-} else {
-	echo 'File Extraction Failed';
-}
+} else echo 'File Extraction Failed';
 
-// rename
+
+// rename and then delete the zip file
 rename( $data_path.$project_name.'-master', $data_path.$project_name );
+unlink ( $theme_file_path );
 
-// delete
-unlink ( $file_path );
+
+//FTP file/directory upload/transfer
+include('phpseclib/NET/SFTP.php');
+include_once('phpseclib/Math/BigInteger.php');
+include_once('phpseclib/Crypt/Random.php');
+include_once('phpseclib/Crypt/Hash.php');
+
+$sftp = new Net_SFTP($host);
+if (!$sftp->login($id, $password)) {
+    exit('Login Failed');
+}
+foreach (glob($data_path.$project_name."/*.*") as $filename) $sftp->put($filename, $dir.$project_name, NET_SFTP_LOCAL_FILE);
 
 	
 /** sample
 
+CODE 1: (Binary Safe based on php.net ) file_get_contents, http://ca2.php.net/file_get_contents
 	$file = file_get_contents($url);
 	$data_path = G5_PATH.'/data/tmp/';
 	$file_path = $data_path.$project_name.'.zip';
 	file_put_contents($file_path, $file);
-
 	
 	$zip = new ZipArchive;
 	if ($zip->open($file_path) == TRUE) {
@@ -51,9 +58,6 @@ unlink ( $file_path );
 	
 	unlink ( $file_path );
  
-	
-http://ca2.php.net/file_get_contents
-
 // download $url and save it into a file(templorary folder like G5/data folder or /tmp foder) or variable.
 
 
