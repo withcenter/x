99f555113ac879e273eb8bff2b8652ed3dd8ac8e
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-03-28 22:16:27 +0800

    Extended_2 Theme

---
+++ theme/extended_2/content.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="<?=x::theme_url()?>/css/content.css">
+
+<table cellspacing='0' cellpadding='0' width='100%' class='content-table'>
+	<tr valign='top'>
+		<td class='left-content'>
+			<div class='left-main-banner'>
+				<img src="<?=x::theme_url('img/left_main_banner.png')?>"/>
+			</div>
+		</td>
+		<td width='20' class='content-divider'></td>
+		<td class='right-content'>
+			
+		</td>
+	</tr>
+</table>
+
+
