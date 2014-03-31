d03ed7e23242a63a22232ca2c805f1cecf91c578
yonggyo00 <lyonggyo@gmail.com>
2014-03-31 16:00:59 +0800

    admin_global_setting

--- module/admin/admin_global_setting.php
+++ module/admin/admin_global_setting.php
@@ -3,21 +3,15 @@
 
 
 
-
+<div class='admin-global-setting'>
 <div class='page-title'>GLOBAL SETTING</div>
 
-
-
-
-
 <form action='?'>
-<input type='hidden' name='module' value='admin'>
-<input type='hidden' name='action' value='admin_global_setting_submit'>
-<div>
-Site CSS & JS Version : <input type='text' name='site_css_js_version' value="<?=x::$config['global']['site_css_js_version']?>" size=4>
-</div>
+	<input type='hidden' name='module' value='admin'>
+	<input type='hidden' name='action' value='admin_global_setting_submit'>
+	<span class='sub-title'>Site CSS & JS Version</span><input type='text' name='site_css_js_version' value="<?=x::$config['global']['site_css_js_version']?>" size=4>
 
-<div>
 	<input type='submit' value='UPDATE'>
-</div>
+	<div style='clear:right;'></div>
 </form>
+</div>
