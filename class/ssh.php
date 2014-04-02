c3b007bb4404abe217190a443b06d5f395c1cedf
JaeHo Song <thruthesky@gmail.com>
2014-04-02 20:05:34 +0900

    working on simple update

--- class/ssh.php
+++ class/ssh.php
@@ -2,8 +2,11 @@
 
 class ssh
 {
+	const SUCCESS = 0;
 	const CONNECTION_FAILED = -1;
 	const LOGIN_FAILED = -2;
+	const SEND_FAILED = -3;
+	const SOURCE_NOT_FOUND = -4;
 	
 	/**
 	 *  @brief copy local files to remote server.
@@ -19,36 +22,38 @@ class ssh
 	 */
 	static function copy( $host, $id, $pw, $path_src, $path_dst )
 	{
-		@$connection = ssh2_connect($host, 22);
+		if ( ! file_exists( $path_src ) ) return self::SOURCE_NOT_FOUND;
+		$connection = ssh2_connect($host, 22);
 		if ( ! $connection ) return self::CONNECTION_FAILED;
-		@$re = ssh2_auth_password($connection, $id, $pw);
+		$re = ssh2_auth_password($connection, $id, $pw);
 		if ( ! $re ) return self::LOGIN_FAILED;
 		if ( is_file( $path_src ) ) {
-				$re = ssh2_scp_send( $connection, $path_src, $path_dst, 0755 );
-				return $re;
+			$re = ssh2_scp_send( $connection, $path_src, $path_dst, 0755 );
+			if ( $re ) return self::SUCCESS;
+			else return self::SEND_FAILED;
 		}
 		else if ( is_dir( $path_src ) ) {
 				chdir( $path_src );
 				$files = getFiles( $path_src );
 				foreach( $files as $file ) {
-						$file = preg_replace("/^\.\//", '', $file);
-						$path = "$path_dst/$file";
-						$pp = pathinfo($path);
-						$dir = $pp['dirname'];
+					$file = preg_replace("/^\.\//", '', $file);
+					$path = "$path_dst/$file";
+					$pp = pathinfo($path);
+					$dir = $pp['dirname'];
 
-						// creating folder
-						$stream = ssh2_exec( $connection, "mkdir -p $dir" );
-						$errorStream = ssh2_fetch_stream($stream, SSH2_STREAM_STDERR);
-						stream_set_blocking($errorStream, true);
-						stream_set_blocking($stream, true);
-						$stdout = stream_get_contents($stream);
-						$stderr = stream_get_contents($errorStream);
-						if ( !empty($stderr) ) return "failed on creating folder: $stderr";
+					// creating folder
+					$stream = ssh2_exec( $connection, "mkdir -p $dir" );
+					$errorStream = ssh2_fetch_stream($stream, SSH2_STREAM_STDERR);
+					stream_set_blocking($errorStream, true);
+					stream_set_blocking($stream, true);
+					$stdout = stream_get_contents($stream);
+					$stderr = stream_get_contents($errorStream);
+					if ( !empty($stderr) ) return "failed on creating folder: $stderr";
 
-						$re = ssh2_scp_send( $connection, $file, $path, 0755 );
-						if ( ! $re ) return "failed on sending $path";
+					$re = ssh2_scp_send( $connection, $file, $path, 0755 );
+					if ( ! $re ) return self::SEND_FAILED;
 				}
-				return $re;
+			return self::SUCCESS;
 		}
 	}
 
