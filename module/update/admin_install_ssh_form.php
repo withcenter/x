7ef68518480741f59dff64e7a7b9af945a6b2025
g5x <g5x@server8.(none)>
2014-04-02 17:47:25 +0900

    renaming

--- module/update/admin_install_ssh_form.php
+++ module/update/admin_install_ssh_form.php
@@ -6,15 +6,14 @@
 <input type='hidden' name='project_name' value='<?=$pname?>'>
 
 <div class='ftp_install'>
+	<h1>SSH INSTALL</h1>
 	<div class='ftp_install_fields'>
-	<h1>FTP INSTALL</h1>
 		Host : <input type='text' name='host' value='<?=etc::domain()?>'>
 		<br>
 		X Folder : <input type='text' name='dir' value='<?=x::dir()?>' style='width: 180px;'><br>
 
-		FTP ID: <input type='text' name='id'><br>
-		FTP Password: <input type='text' name='password'><br>
-		Port : 22 - SFTP
+		SSH(SFTP) ID: <input type='text' name='id'><br>
+		SSH(SFTP) Password: <input type='text' name='password'><br>
 		<br>
 		<input type='submit'>
 	</div>
