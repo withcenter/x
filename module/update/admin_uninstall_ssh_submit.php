084cec28fcdce5eb2d603ed0d4ecb9e956856cee
JaeHo Song <thruthesky@gmail.com>
2014-04-03 20:52:39 +0900

    working on simple update

--- module/update/admin_uninstall_ssh_submit.php
+++ module/update/admin_uninstall_ssh_submit.php
@@ -1,14 +1,11 @@
 <?php
-
 $vars['host']			= $host;
 $vars['id']				= $id;
 $vars['password']		= $password;
 $vars['dir']			= $dir;
 $vars['type']			= $type;
 $vars['name']			= $name;
-
 $var = http_build_query( $vars );
-
 ?>
 <link rel="stylesheet" href="<?=module("$module.css")?>">
 <script src="<?=module("$module.js")?>"></script>
@@ -22,9 +19,7 @@ $(function(){
 		data: data,
 		callback: 'callback_ajax_call'
 	} );
-
 	// callback_ajax_call( { 'code' : '0' } ); // TEST CALL
-
 });
 function callback_ajax_call( re )
 {
@@ -34,29 +29,30 @@ function callback_ajax_call( re )
 		$('.loader').remove();
 		$('.result.success').show();
 	}
+	else {
+		alert( "ERROR: " + re.message );
+		$('.loader').remove();
+		$('.result.error').show();
+	}
 }
 
 </script>
 
 
-
-
 <div class='ssh-install-submit'>
-
 	<div class='loader'>
 		<img src="<?=x::url()?>/img/loader12.gif">
 		<?=ln("Please, wait while theme is being un-installed.",
 			"테마를 삭제 중입니다. 기다려 주십시오.")?>
 	</div>
-	
-	
 	<div class='result success'>
 		<?=ln("Finished. The theme was un-installed succfully.",
 			"완료. 테마가 성공적으로 삭제되었습니다.")?>
 	</div>
-	
-	
-	
+	<div class='result success'>
+		<?=ln("Finished. The theme was un-installed succfully.",
+			"완료. 테마가 성공적으로 삭제되었습니다.")?>
+	</div>
 </div>
 
 
