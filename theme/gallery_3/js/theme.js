78a0c014fb200b8bc516ec169389e6c37593ff35
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-04-04 15:55:03 +0800

    menu fixes and banner rotate fix

--- theme/gallery_3/js/theme.js
+++ theme/gallery_3/js/theme.js
@@ -1,8 +1,20 @@
 var banner_count = 0;
 var curr_banner = 1;
 $(function(){
-
-	$(".header .top-search .menu-dropdown-button").click(function(){
+	$(window).resize(function(){		
+		if( $(window).width() > 1007 ){
+			if( !$(".header .top-menu").hasClass('has_1007_width') ){
+				$(".header .top-menu").show();				
+			}
+			$(".header .top-menu").addClass('has_1007_width');
+		}else{
+			if( $(".header .top-menu").hasClass('has_1007_width') ){				
+				$(".header .top-menu").removeClass('has_1007_width');
+			}
+		}
+	});
+	
+	$(".header .top-search .menu-dropdown-button").click(function(){	
 		$(".header .top-menu").toggle();
 		$(".header .triangle-border").toggle();
 		$(".header .triangle").toggle();
@@ -167,9 +179,9 @@ $(window).resize(function(){
 	
 /***************BANNER_ROTATION******************/
 	var stopped = false;
-	
-	var banner_interval = setInterval(function(){
-	if( banner_count < 1 ) return;
+	console.log(banner_count);	
+	var banner_interval = setInterval(function(){	
+		if( banner_count < 1 ) return;
 		do_rotate();
 	},4000);
 	
