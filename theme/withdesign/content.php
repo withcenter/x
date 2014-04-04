8f28f5919e841fa7a7f7a30b0dc8efe6447720ad
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-04-04 16:14:01 +0800

    withdesign theme structure

---
+++ theme/withdesign/content.php
@@ -0,0 +1,22 @@
+<div class='services-banner'>
+	<div class='banner-container'>
+		<div class='banner'>
+			<div class='banner-title'>
+				BANNER TITLE
+			</div>
+			<div class='banner-image'>
+			
+			</div>
+			<div class='view-banner'>
+			
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<style>
+	.services-banner {
+		background: url("<?=x::theme_url('img/banner_background.png')?>");
+	}
+</style>
---
+++ theme/withdesign/tail.php
@@ -0,0 +1,8 @@
+
+		</div><!--content-->
+	</div><!--content-wrapper-->
+	<div class='footer-wrapper'>
+		<div class='footer'>
+		
+		</div><!--footer-->
+	</div><!--footer-wrapper-->
