b450df55bf78027c0f2c3e55a41f692ef60d0b0b
yonggyo00 <lyonggyo@gmail.com>
2014-04-07 22:24:24 +0800

    xe_style

--- theme/xe_style/multitab.middle.latest.php
+++ theme/xe_style/multitab.middle.latest.php
@@ -1,6 +1,8 @@
 <?php
 add_stylesheet("<link rel='stylesheet' type='text/css' href='".x::url_theme()."/css/multitab.middle.latest.css' />", 0);
-
+?>
+<script src='<?=x::url_theme()?>/js/multitab.middle.latest.js'></script>
+<?php
 include_once(G5_LIB_PATH.'/thumbnail.lib.php');
 $mmposts = array();
 for ( $i=6; $i < 9; $i++ ) {
@@ -12,29 +14,42 @@ for ( $i=6; $i < 9; $i++ ) {
 	);
 	$mmposts[] = g::posts($option);
 }
-echo "<div class='multitab-middle-latest'>";
+echo "<div class='multitab-middle-latest'>
+		<span class='title'>GNUXE HUMOR</span>
+		<ul class='menu-tab'>
+			<li menu=2 class='last-menu'>메뉴2</li>
+			<li menu=1>메뉴1</li>
+			<li menu=0>메뉴0</li>
+		</ul>
+		<div style='clear:both;'></div>
+";
+
+
+
 for ( $l=0; $l< 3; $l++ ) {
-echo "<table class='tab-bottom' cellpsdding=0 cellspacing=0 width='100%' border=1>
-		<tr class='row'>";
+	if ( $l == 0 ) $first_row = "first-row";
+	else $first_row = null;
+	
+	echo "<div class='row $first_row' menu-content={$l}>";
 	$m = 0;
 	foreach ( $mmposts[$l] as $p ) {
+		$wr_subject = conv_subject( $p['wr_subject'], 12, '...');
 		$imgsrc = get_list_thumbnail($p['bo_table'], $p['wr_id'], 105, 69);
 		if ( $imgsrc['src'] ) {
 			$img = "<img src='$imgsrc[src]'/>";
 		} else if ( $image_from_tag = g::thumbnail_from_image_tag( $p['wr_content'], $p['bo_table'], 105, 69 )) {
 		$img = "<img class='img_left' src='$image_from_tag'/>";
-		} else $img = "<img src='".x::url_theme()."/img/no_image.png'";
+		} else $img = "<img src='".x::url_theme()."/img/no_image.png' />";
 		
 		$url = G5_BBS_URL . '/board.php?bo_table='.$p['bo_table'].'&wr_id='.$p['wr_id'];
 		
-		echo "<td width='20%'><div class='photo'><a href='$url'>$img</a></div></td>";
+		echo "<div class='photo'><a href='$url'>$img</a></div>";
 		$m++;
 	}
-		for ( $i=0; $i < (5 - $m ); $i++ ) {
-			echo "<td width='20%'><img src='".x::url_theme()."/img/no_image.png' /></td>";
+		for ( $i=0; $i < ( 5 - $m ); $i++ ) {
+			echo "<div class='photo'><img src='".x::url_theme()."/img/no_image.png' /></div>";
 		}
-	echo "</tr>
-		</table>
-	";
+	echo "<div style='clear:left;'></div>
+		</div>";
 }
 echo "</div>";
