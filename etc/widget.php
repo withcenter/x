76f0756db047fdbfd624b2e6c1de050401e7c28f
JaeHo Song <thruthesky@gmail.com>
2014-04-14 19:49:13 +0900

    updating submodule widget: comment-line

--- etc/widget.php
+++ etc/widget.php
@@ -3,8 +3,15 @@
  *
  */
 	load_widget_config( $widget_config['code'] );
+?>
+<div class="widget-admin" code="<?=$widget_config['code']?>" name="<?=$widget_config['name']?>">
+<?	
+	
+
+	
 	
 	$widget_script = x::dir() . "/widget/$widget_config[name]/$widget_config[name].php";
+	
 	if ( ! file_exists( $widget_script ) ) {
 		echo "
 			<div style='margin-bottom: 1em; padding: 1em; border: 1px solid red; overflow:hidden; word-break: break-all; line-height: 140%;'>
@@ -19,17 +26,19 @@
 		}
 		echo "</div>";
 		
-		
-		
-		return;
 	}
+	else {
 	
 	x::hook("widget_begin: code=$widget_config[code], name=$widget_config[name]");
 ?>
-<div class="widget-admin" code="<?=$widget_config['code']?>" name="<?=$widget_config['name']?>">
+
 	<?php
 		include $widget_script;
 	?>
-</div>
 <?php x::hook("widget_end"); ?>
+<?
+	}
+?>
+
+</div>
 	
