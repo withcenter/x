835bb4b2b501c115db3695c0ff4da26b8837a24c
JaeHo Song <thruthesky@gmail.com>
2014-04-14 19:22:06 +0900

    updating widget

--- etc/widget.php
+++ etc/widget.php
@@ -6,7 +6,21 @@
 	
 	$widget_script = x::dir() . "/widget/$widget_config[name]/$widget_config[name].php";
 	if ( ! file_exists( $widget_script ) ) {
-		echo "<div style='margin-bottom: 1em; padding: 1em; border: 1px solid red;'>No widget script : $widget_config[name]</div>";
+		echo "
+			<div style='margin-bottom: 1em; padding: 1em; border: 1px solid red; overflow:hidden; word-break: break-all; line-height: 140%;'>
+				No widget : $widget_config[name]
+		";
+		if ( $widget_config['git'] ) {
+			$url = "?module=update&action=admin_install&type=widget&source_link=" . urlencode( $widget_config['git'] );
+			echo "
+					<br>
+					<a href='$url'>INSTALL : $widget_config[git]</a>
+			";
+		}
+		echo "</div>";
+		
+		
+		
 		return;
 	}
 	
