81d8884f0fd889ff7ce7d002af8bcc11da208cdf
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-04-15 17:36:55 +0800

    gallery-3 to git

--- theme/gallery_3/setting_submit.php
+++
@@ -1,7 +0,0 @@
-<?
-	for( $i=1; $i<=5; $i++) {
-		x::meta("banner{$i}_subject", $in["banner{$i}_subject"]);
-		x::meta("banner{$i}_content", $in["banner{$i}_content"]);	
-		x::meta("banner{$i}_url", $in["banner{$i}_url"]);
-	}
-	
