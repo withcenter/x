f9247c36866cf549332a09172351c648b131e87a
JaeHo Song <thruthesky@gmail.com>
2014-04-15 19:58:19 +0900

    working on widget

---
+++ module/widget/config.height.php
@@ -0,0 +1,7 @@
+<?php
+	include widget_config_form('text', array(
+			'name'				=> 'height',
+			'caption'			=> ln('Height', '높이'),
+			'placeholder'	=> ln("Input height", "높이를 입력하십시오.")
+		)
+	);
---
+++ module/widget/config.width.php
@@ -0,0 +1,7 @@
+<?php
+	include widget_config_form('text', array(
+			'name'				=> 'width',
+			'caption'			=> ln('Width', '너비'),
+			'placeholder'	=> ln("Input width", "너비를 입력하십시오.")
+		)
+	);
