1856c2e7c7a2bffd4f238ef24dc21cef51c8c98b
JaeHo Song <thruthesky@gmail.com>
2014-04-17 19:28:57 +0900

    simple update: chaning into new scheme

--- module/update/dist-menu.php
+++ module/update/dist-menu.php
@@ -5,10 +5,10 @@
 <a href='?module=update&action=<?=$pre?>list'>SEARCH Source</a>
 |
 <? if ( admin() ) { ?>
-<a href='?module=update&action=list_installed'>INSTALLED Source</a>
+<a href='?module=update&action=admin_installed_list'>INSTALLED Source</a>
 |
 <? } ?>
-<a href='<?=X_URL_REAL?>?module=update&action=source'>UPLOAD Source</a>
+<a href='<?=X_URL_REAL?>?module=update&action=source' target='_blank'>UPLOAD Source</a>
 </div>
 <style>
 .dist-menu [href*='<?=$action?>'] {
