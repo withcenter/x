7cba5aae5d9f57ebacbc890f571c64b0a5a5cb53
yonggyo00 <lyonggyo@gmail.com>
2014-04-22 21:24:27 +0900

    updating site list for philgo

--- module/etc/site_list_for_philgo.php
+++ module/etc/site_list_for_philgo.php
@@ -1,12 +1,16 @@
 <?php
 include x::dir().'/etc/share/head.php';
 ?>
-
+<style>
+table tr td {
+	padding: 1em;
+}
+</style>
 	<table cellpadding=0 cellspacing=0 width='100%' class='admin-list-table'>
 		<tr valign='top' class='header'>
-			<td>Domain</td>
-			<td>Site Name</td>
-			<td align='center'>Post</td>
+			<td><b>홈페이지 주소</b></td>
+			<td><b>사이트 이름</b></td>
+			<td align='center'><b>게시글 수</b></td>
 		</tr>
 		<?php
 			$sites = db::rows("SELECT * FROM x_site_config WHERE good > 0 ORDER BY good DESC, domain ASC LIMIT 20");
