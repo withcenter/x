0aa76d1566a2eb1ab3c15694071f77c5080dfb13
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-04-24 22:00:42 +0800

    footer space

--- theme/community_3/tail.php
+++ theme/community_3/tail.php
@@ -12,19 +12,19 @@
 							if ( $footer_text = meta('footer_text') ) echo nl2br($footer_text); 
 							else echo "어드민 페이지에서 하단 문구를 입력해 주세요";
 						?>
-					</div>					
-				</div>			
-				<div class='comm3_visit_stats'>
-					<?
-						include widget(
-							array(
-								'code'		=> 'stat-visit',
-								'name'		=> 'stat-visit',
-								'git'		=> 'https://github.com/x-widget/stat-visit',
-							)
-						);
-					?>
-				</div>				
+					</div>
+					<div class='comm3_visit_stats'>
+						<?
+							include widget(
+								array(
+									'code'		=> 'stat-visit',
+									'name'		=> 'stat-visit',
+									'git'		=> 'https://github.com/x-widget/stat-visit',
+								)
+							);
+						?>
+					</div>
+				</div>								
 			</div>							
 		</div><!--footer-->
 </div><!--layout-->
