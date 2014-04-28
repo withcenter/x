08d778cbea240e8c7b07835c5566107e1787f071
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-04-28 21:21:51 +0800

    finishing touches for the new default theme

--- theme/default/post-list.php
+++ theme/default/post-list.php
@@ -36,11 +36,10 @@ $total_posts = count($posts);
 	<div class='listed_posts right'>
 		<div class='inner'>
 			<?		
-			for( $i2 = ceil($total_posts/2); $i2 <= $total_posts; $i2++ ) {
-				$subject = conv_subject($posts[$i2]['wr_subject'], 15, "...");	
-				$wr_url =site_url($posts[$i2]['domain']) . "/bbs/board.php?bo_table=" . $posts[$i2]['bo_table'] . "&wr_id=".$posts[$i2]['wr_id'];
-				if( ($i2+1) == $total_posts ) $last_post = 'last_post';			
-				//di( $post_count." ".$total_posts );
+			for( $i = ceil($total_posts/2)+1; $i <= $total_posts; $i++ ) {			
+				$subject = conv_subject($posts[$i]['wr_subject'], 15, "...");	
+				$wr_url =site_url($posts[$i]['domain']) . "/bbs/board.php?bo_table=" . $posts[$i]['bo_table'] . "&wr_id=".$posts[$i]['wr_id'];
+				if( ($i+1) == $total_posts ) $last_post = 'last_post';				
 			?>	
 						<div class='list_item <?=$last_post?>'>
 							<a href='<?=$wr_url?>' target='_blank'><?=$subject?></a>
