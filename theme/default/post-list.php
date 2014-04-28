0eb99cb32dad69e62551344d575eab12fbf6298d
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-04-28 22:14:26 +0800

    tablet layout fixes

--- theme/default/post-list.php
+++ theme/default/post-list.php
@@ -25,7 +25,7 @@ $total_posts = count($posts);
 			for( $i = 0; $i < ceil($total_posts/2); $i++ ) {
 				$subject = conv_subject($posts[$i]['wr_subject'], 15, "...");	
 				$wr_url =site_url($posts[$i]['domain']) . "/bbs/board.php?bo_table=" . $posts[$i]['bo_table'] . "&wr_id=".$posts[$i]['wr_id'];
-				if( ($i + 1) >= $total_posts/2 ) $last_post = 'last_post';	
+				if( ($i + 1) >= $total_posts/2 ) $last_post = 'last_post left';	
 			?>	
 						<div class='list_item <?=$last_post?>'>
 							<a href='<?=$wr_url?>' target='_blank'><?=$subject?></a>
