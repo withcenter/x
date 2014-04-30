390013dec1703320f6e9b61c44634672fb504b31
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-04-30 15:41:00 +0800

    quick fixeS

--- widget/login-default/login-form.php
+++ widget/login-default/login-form.php
@@ -16,29 +16,9 @@
 		</div>
 		<div class='login-divider'></div>
 		<div class='register-lost-password'>
-			<a href="<?=url_bbs()?>/register.php">회원가입</a> ~ 	<a href="<?=url_bbs()?>/password_lost.php">정보찾기</a>
+			<a href="<?=url_bbs()?>/register.php">회원가입</a> ~ <a href="<?=url_bbs()?>/password_lost.php">정보찾기</a>
 		</div>
 	</div>
-	<!--<table cellpadding='0' cellspacing='0' width='100%'>
-		<tr>
-			<td>
-				<input type="text" name="mb_id" class="id" maxlength="20" placeholder="<?=ln("User ID", "아이디")?>">
-				<input type="password" name="mb_password" class="password" maxlength="20" placeholder="<?=ln("Password", "비밀번호")?>">
-			</td>
-			<td><input type="submit" value="로그인"></td>
-		</tr>
-		<tr>
-			<td colspan=2>
-				<div class='bottom'>
-            <a href="<?=url_bbs()?>/register.php"><b>회원가입</b></a>
-            <a href="<?=url_bbs()?>/password_lost.php">정보찾기</a>
-			
-            <input type="checkbox" name="auto_login" value="1" id="auto_login">
-            <label for="auto_login" id="auto_login_label">자동로그인</label>
-			</div>
-			</td>
-		</tr>
-	</table>-->
 </form>
 </div>
 
