7ded53537b42f3ca7002edb6cf58fff4e064a0fb
ontuebenjamin <ontuebenjamin@gmail.com>
2014-07-04 19:18:57 +0800

    board skins add_stylesheet to <link> tag

--- skin/board/multi/view.skin.php
+++ skin/board/multi/view.skin.php
@@ -3,8 +3,10 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 include_once(G5_LIB_PATH.'/thumbnail.lib.php');
 
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
-add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0);
+//add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0);
+//add_stylesheet('<link rel="stylesheet" href="'.G5_URL.'/x/skin/board/multi/style.css">', 0);
 ?>
+<link rel="stylesheet" href="<?=$board_skin_url?>/style.css">
 
 <script src="<?php echo G5_JS_URL; ?>/viewimageresize.js"></script>
 
